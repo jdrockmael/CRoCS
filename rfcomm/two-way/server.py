5792b954917774ab59ffee24e0c13b12a6e98357
jdrockmael <jdrockmael@wpi.edu>
2023-12-02 14:20:28 -0500

    removed try/catch

--- rfcomm/two-way/server.py
+++ rfcomm/two-way/server.py
@@ -7,14 +7,16 @@ size = 1024
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.bind((hostMACAddress, port))
 s.listen(backlog)
-try:
-    client, clientInfo = s.accept()
-    while 1:
-        data = client.recv(size)
-        if data:
-            print(data)
-            client.send(data) # Echo back to client
-except:	
-    print("Closing socket")
-    client.close()
-    s.close()
+
+client, clientInfo = s.accept()
+while 1:
+    data = client.recv(size)
+    if data:
+        print(data)
+        client.send(data) # Echo back to client
+    if data == 'quit':
+        break
+
+print("Closing socket")
+client.close()
+s.close()
