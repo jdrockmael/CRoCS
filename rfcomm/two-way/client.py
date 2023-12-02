6830cac5e8657c2ac90fe184b6d451e7375d9bbe
jdrockmael <jdrockmael@wpi.edu>
2023-12-02 14:17:31 -0500

    added two way test

---
+++ rfcomm/two-way/client.py
@@ -0,0 +1,12 @@
+import bluetooth
+
+serverMACAddress = 'B8:27:EB:72:37:1B'
+port = 3
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.connect((serverMACAddress, port))
+while 1:
+    text = input()
+    if text == "quit":
+        break
+    s.send(text)
+s.close()
---
+++ rfcomm/two-way/server.py
@@ -0,0 +1,20 @@
+import bluetooth
+
+hostMACAddress = 'B8:27:EB:72:37:1B' # The MAC address of a Bluetooth adapter on the server. The server might have multiple Bluetooth adapters.
+port = 3
+backlog = 1
+size = 1024
+s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+s.bind((hostMACAddress, port))
+s.listen(backlog)
+try:
+    client, clientInfo = s.accept()
+    while 1:
+        data = client.recv(size)
+        if data:
+            print(data)
+            client.send(data) # Echo back to client
+except:	
+    print("Closing socket")
+    client.close()
+    s.close()
