b5aae1b96ff55a45a39ca2e9e7e6d3e089f6e656
jdrockmael <jdrockmael@wpi.edu>
2023-12-02 13:26:13 -0500

    multi server code

--- rfcomm/rfcomm-server.py
+++ rfcomm/rfcomm-server.py
@@ -2,15 +2,22 @@ import bluetooth
 
 server_sock=bluetooth.BluetoothSocket( bluetooth.RFCOMM )
 
-port = 1
+port = 3
 server_sock.bind(("",port))
-server_sock.listen(1)
+server_sock.listen(2)
 
-client_sock,address = server_sock.accept()
-print("Accepted connection from ",address)
+client_sock1,address1 = server_sock.accept()
+print("Accepted connection from ",address1)
 
-data = client_sock.recv(1024)
-print("received [%s]" % data)
+client_sock2,address2 = server_sock.accept()
+print("Accepted connection from ",address2)
 
-client_sock.close()
+data1 = client_sock1.recv(1024)
+print("received [%s]" % data1, " from ", address1)
+
+data2 = client_sock2.recv(1024)
+print("received [%s]" % data2, " from ", address2)
+
+client_sock1.close()
+client_sock2.close()
 server_sock.close()
