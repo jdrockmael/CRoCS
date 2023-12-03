8bc92af7384ede75093d519058d7305ffa71167b
Timothy Klein <oitsplazma444@gmail.com>
2023-12-03 17:45:45 -0500

    final client didn't need much work

--- rfcomm/final/client.py
+++ rfcomm/final/client.py
@@ -6,15 +6,7 @@ port = 3
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 
-text = input()
-s.send(text)
-
 while 1:
-    data = s.recv(1024)
-    if data:
-        print(data)
-        if data == b'quit':
-            break 
         text = input()
         s.send(text)
         if text == "quit":
