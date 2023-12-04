b0357897c0e2865ed6435607991f335dd7051a20
Benji002 <Benji202002@gmail.com>
2023-12-04 12:24:50 -0500

    Update centroids.py
    Rearranged for optimization

--- centroids.py
+++ centroids.py
@@ -4,24 +4,24 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+# define range of blue color in HSV
+lower_blue = np.array([104, 100, 225])
+upper_blue = np.array([130, 255, 255])
+
+# define range of red color in HSV
+lower_red = np.array([130, 165, 225])
+upper_red = np.array([179, 255, 255])
+
+# define range of green color in HSV
+lower_green = np.array([62, 110, 225])
+upper_green = np.array([84, 215, 255])
+
 while(1):
     # Take each frame
     _, frame = cap.read()
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-
-    # define range of blue color in HSV
-    lower_blue = np.array([104, 100, 225])
-    upper_blue = np.array([130, 255, 255])
-
-    # define range of red color in HSV
-    lower_red = np.array([130, 165, 225])
-    upper_red = np.array([179, 255, 255])
-
-    # define range of green color in HSV
-    lower_green = np.array([52, 110, 225])
-    upper_green = np.array([84, 215, 255])
 
     # Threshold the HSV image to get only blue colors
     mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
