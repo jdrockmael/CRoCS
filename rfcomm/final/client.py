337fdbd09577cf40f701e56a9113f9cb24568be6
Timothy Klein <oitsplazma444@gmail.com>
2023-12-10 12:31:25 -0500

    made final client multi threaded but unable to test sorry

--- rfcomm/final/client.py
+++ rfcomm/final/client.py
@@ -1,4 +1,5 @@
 import bluetooth
+import threading
 
 serverMACAddress = '28:D0:EA:60:BC:E6'
 
@@ -6,13 +7,29 @@ port = 4
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 
-while 1:
-        data = s.recv(1024)
+exit_event = threading.Event()
+
+def rec_msg(socket, event):
+     while not event.is_set():
+        data = socket.recv(1024)
         if data:
             print(data)
 
-        text = input()
-        s.send(text)
-        if text == "quit":
-            break
+def send_msg(socket, event):
+    while True:
+            text = input()
+            socket.send(text)
+            if text == "quit":
+                event.set()
+                break
+
+rec_thread = threading.Thread(target=rec_msg, args=(s, exit_event))
+send_thread = threading.Thread(target=send_msg, args=(s, exit_event))
+
+rec_thread.start()
+send_thread.start()
+
+rec_thread.join()
+send_thread.join()
+
 s.close()
