e5d08ca57015a4a0791d2d1600ef71a102b0d370
Timothy Klein <oitsplazma444@gmail.com>
2023-12-18 17:47:32 -0500

    reworked the client final to fix the quiting bug

--- rfcomm/final/client.py
+++ rfcomm/final/client.py
@@ -7,27 +7,27 @@ port = 4
 s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 s.connect((serverMACAddress, port))
 
-exit_event = threading.Event()
+event = False
 
-def rec_msg(socket, event):
-     while not event.is_set():
+def rec_msg(socket):
+     while not event:
         print("waiting to recieve data")
         data = socket.recv(1024)
         if data:
             print(data)
 
-def send_msg(socket, event):
+def send_msg(socket):
     while True:
             print("ready to take input")
             text = input()
             socket.send(text)
 
             if text == "quit":
-                event.set()
+                event = True
                 break
 
-rec_thread = threading.Thread(target=rec_msg, args=(s, exit_event))
-send_thread = threading.Thread(target=send_msg, args=(s, exit_event))
+rec_thread = threading.Thread(target=rec_msg, args=(s))
+send_thread = threading.Thread(target=send_msg, args=(s))
 
 rec_thread.start()
 send_thread.start()
