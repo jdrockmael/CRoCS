2324170a4cf7c9eb5e37bb9652a598c2156ab313
Dang Tran <dttran@wpi.edu>
2024-01-12 14:13:18 -0500

    Orientation Yaw test v1.0

--- april.py
+++ april.py
@@ -2,6 +2,7 @@ import cv2
 from dt_apriltags import Detector
 import numpy as np
 import glob
+import math
 
 visualization = True
 try:
@@ -16,39 +17,37 @@ except:
     print("Visualization will be disabled.")
     visualization = False
 
-# try:
-#     import yaml
-# except:
-#     raise Exception('You need yaml in order to run the tests. However, you can still use the library without it.')
+try:
+    import yaml
+except:
+    raise Exception('You need yaml in order to run the tests. However, you can still use the library without it.')
 
 
 LINE_LENGTH = 5
 CENTER_COLOR = (0, 255, 0)
 CORNER_COLOR = (255, 0, 255)
 
-#criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-#objp = np.zeros((6*7,3), np.float32)
-#objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 # Arrays to store object points and image points from all the images.
-#objpoints = [] # 3d point in real world space
-#imgpoints = [] # 2d points in image plane.
-#images = glob.glob('calib_data/*.jpg')
-#cam_param = []
-#for fname in images:
-#    img = cv2.imread(fname)
-#    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Find the chess board corners
-#    ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('calib_data/*.jpg')
+cam_param = []
+for fname in images:
+   img = cv2.imread(fname)
+   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
     # If found, add object points, image points (after refining them)
-#    if ret == True:
-#        objpoints.append(objp)
-#        corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-#        imgpoints.append(corners2)
+   if ret == True:
+       objpoints.append(objp)
+       corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+       imgpoints.append(corners2)
 
-#        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-#        cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
-#print(cam_param)
+       ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+       cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
+print(cam_param)
 ### Some utility functions to simplify drawing on the camera feed
 # draw a crosshair
 def plotPoint(image, center, color):
@@ -72,16 +71,16 @@ def plotText(image, center, color, text):
                        1, color, 3)
 
 cap = cv2.VideoCapture(0)
-# at_detector = Detector(families='tag36h11',
-#                        nthreads=1,
-#                        quad_decimate=1.0,
-#                        quad_sigma=0.0,
-#                        refine_edges=1,
-#                        decode_sharpening=0.25,
-#                        debug=0)
+at_detector = Detector(families='tag36h11',
+                       nthreads=1,
+                       quad_decimate=1.0,
+                       quad_sigma=0.0,
+                       refine_edges=1,
+                       decode_sharpening=0.25,
+                       debug=0)
 
-# with open('april_param.yaml', 'r') as stream:
-#     parameters = yaml.safe_load(stream)
+with open('april_param.yaml', 'r') as stream:
+    parameters = yaml.safe_load(stream)
 
 #parameters = []
 
@@ -94,11 +93,10 @@ while(1):
     grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     img = grayscale
-    #cameraMatrix = np.array(parameters['sample_test']['K']).reshape((3,3))
-    #camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
-    # print(camera_params)
-    #tags = at_detector.detect(img, True, camera_params, parameters['sample_test']['tag_size'])
-    #tags =at_detector.detect(img, estimate_tag_pose=True, camera_params=cam_param, tag_size=5)
+    # cameraMatrix = np.array(parameters['sample_test']['K']).reshape((3,3))
+    # camera_params = ( cameraMatrix[0,0], cameraMatrix[1,1], cameraMatrix[0,2], cameraMatrix[1,2] )
+    # tags = at_detector.detect(img, True, camera_params, parameters['sample_test']['tag_size'])
+    # tags =at_detector.detect(img, estimate_tag_pose=True, camera_params=cam_param, tag_size=5)
     tags = at_detector.detect(img)
 
     # color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
@@ -106,6 +104,12 @@ while(1):
     if tags:
         # print(tags)
         for tag in tags:
+            # print(tag)
+            # print("AAAAAAAAAAAAAAAA", tag.pose_R[1][0])
+            # print("AAAAAAAAAAAAAAAA", tag.pose_R[0][0])
+            # print("YAWWWWWWWWWWWWW", math.degrees(math.atan2(tag.pose_R[1][0],tag.pose_R[0][0])))
+            # print("YAWWWWWWWWWWWWW", math.degrees(math.atan2(-tag.pose_R[2][0],math.sqrt(pow(tag.pose_R[2][1],2) + pow(tag.pose_R[2][2],2)))))
+            # yaw=atan2(R(2,1),R(1,1));
             
             # print("tag_id: %s, center: %s" % (tag.tag_id, tag.center))
             frame = plotPoint(frame, tag.center, CENTER_COLOR)
