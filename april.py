51d443668328ff7dc96e7f5a3fab33f02416ffff
Benji002 <benji202002@gmail.com>
2024-01-13 13:52:16 -0500

    Orientation working

--- april.py
+++ april.py
@@ -27,27 +27,32 @@ LINE_LENGTH = 5
 CENTER_COLOR = (0, 255, 0)
 CORNER_COLOR = (255, 0, 255)
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-images = glob.glob('calib_data/*.jpg')
-cam_param = []
-for fname in images:
-   img = cv2.imread(fname)
-   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
-    # If found, add object points, image points (after refining them)
-   if ret == True:
-       objpoints.append(objp)
-       corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-       imgpoints.append(corners2)
+############### CAMERA CALIBRATION #################
+# Get camera parameters
 
-       ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-       cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
-print(cam_param)
+# criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+# objp = np.zeros((6*7,3), np.float32)
+# objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# # Arrays to store object points and image points from all the images.
+# objpoints = [] # 3d point in real world space
+# imgpoints = [] # 2d points in image plane.
+# images = glob.glob('calib_data/*.jpg')c
+# cam_param = []
+# for fname in images:
+   # img = cv2.imread(fname)
+   # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   # ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    # # If found, add object points, image points (after refining them)
+   # if ret == True:
+       # objpoints.append(objp)
+       # corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+       # imgpoints.append(corners2)
+
+       # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+       # cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
+# print(cam_param)
+
+
 ### Some utility functions to simplify drawing on the camera feed
 # draw a crosshair
 def plotPoint(image, center, color):
@@ -79,13 +84,10 @@ at_detector = Detector(families='tag36h11',
                        decode_sharpening=0.25,
                        debug=0)
 
-with open('april_param.yaml', 'r') as stream:
-    parameters = yaml.safe_load(stream)
-
-#parameters = []
-
 at_detector = Detector()
 
+# Arducam parameters
+cam_param = (534.0708862263613, 534.1191479816413, 341.5340710724817, 232.94565221113476)
 while(1):
 
     # Take each frame
