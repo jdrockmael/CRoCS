256cb592fdf40b363c8560339376e899967970ce
Dang Tran <dttran@wpi.edu>
2024-01-14 12:50:41 -0500

    Clean up and added moving average

--- april.py
+++ april.py
@@ -3,6 +3,7 @@ from dt_apriltags import Detector
 import numpy as np
 import glob
 import math
+import collections
 
 visualization = True
 try:
@@ -27,32 +28,26 @@ LINE_LENGTH = 5
 CENTER_COLOR = (0, 255, 0)
 CORNER_COLOR = (255, 0, 255)
 
-############### CAMERA CALIBRATION #################
-# Get camera parameters
-
-# criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-# objp = np.zeros((6*7,3), np.float32)
-# objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-# # Arrays to store object points and image points from all the images.
-# objpoints = [] # 3d point in real world space
-# imgpoints = [] # 2d points in image plane.
-# images = glob.glob('calib_data/*.jpg')c
-# cam_param = []
-# for fname in images:
-   # img = cv2.imread(fname)
-   # gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-   # ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
-    # # If found, add object points, image points (after refining them)
-   # if ret == True:
-       # objpoints.append(objp)
-       # corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
-       # imgpoints.append(corners2)
-
-       # ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-       # cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
-# print(cam_param)
-
-
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((6*7,3), np.float32)
+objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+# Arrays to store object points and image points from all the images.
+objpoints = [] # 3d point in real world space
+imgpoints = [] # 2d points in image plane.
+images = glob.glob('calib_data/*.jpg')
+cam_param = []
+for fname in images:
+   img = cv2.imread(fname)
+   gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+   ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+    # If found, add object points, image points (after refining them)
+   if ret == True:
+       objpoints.append(objp)
+       corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+       imgpoints.append(corners2)
+       ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+       cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
+print(cam_param)
 ### Some utility functions to simplify drawing on the camera feed
 # draw a crosshair
 def plotPoint(image, center, color):
@@ -76,18 +71,13 @@ def plotText(image, center, color, text):
                        1, color, 3)
 
 cap = cv2.VideoCapture(0)
-at_detector = Detector(families='tag36h11',
-                       nthreads=1,
-                       quad_decimate=1.0,
-                       quad_sigma=0.0,
-                       refine_edges=1,
-                       decode_sharpening=0.25,
-                       debug=0)
-
 at_detector = Detector()
 
 # Arducam parameters
-cam_param = (534.0708862263613, 534.1191479816413, 341.5340710724817, 232.94565221113476)
+# cam_param = (534.0708862263613, 534.1191479816413, 341.5340710724817, 232.94565221113476)
+
+yaw = collections.deque(maxlen=10)
+dist = collections.deque(maxlen=10)
 while(1):
 
     # Take each frame
@@ -103,33 +93,34 @@ while(1):
     if tags:
         # print(tags)
         for tag in tags:
-            # print(tag)
-            # Roll
-            # print("YAWWWWWWWWWWWWW", math.degrees(math.atan2(tag.pose_R[1][0],tag.pose_R[0][0])))
-            print("YAW", math.degrees(math.atan2(-tag.pose_R[2][0],math.sqrt(pow(tag.pose_R[2][1],2) + pow(tag.pose_R[2][2],2)))))
-            # yaw=atan2(R(2,1),R(1,1));
-            
             # print("tag_id: %s, center: %s" % (tag.tag_id, tag.center))
             frame = plotPoint(frame, tag.center, CENTER_COLOR)
-            # frame = plotText(frame, tag.center, CENTER_COLOR, tag.tag_id)
-            # frame = plotText(frame, tag.center, CENTER_COLOR, np.round(tag.pose_t[2], 3))
-            # print(len(tag.corners))
             width = []
             for idx in range(len(tag.corners)):
                 width.append(max(abs(tag.corners[idx-1, :] - tag.corners[idx,:])))
-                # cv2.line(frame, tuple(tag.corners[idx-1, :].astype(int)), tuple(tag.corners[idx, :].astype(int)), (0, 255, 0))
             # print(width)
-            #print(tag.pos_R)
             width = np.average(width)
+
+            # Focal length = (Pixel width x distance) / Actual width
+            # Dist = (Actual Width x Focal Length) / Pixel Width
             #print("FOCAL LENGTH: ", width * 60 / 5)
             #these values work at 1.5inx1.5in 360 and 60
             focal = 360
             manual_dist = focal * 60 / width
-            
-            # print(width)
-            # Focal lenght = (Pixel weidth x dsitance) / Actual width
+            curr_yaw = math.degrees(math.atan2(-tag.pose_R[2][0],math.sqrt(pow(tag.pose_R[2][1],2) + pow(tag.pose_R[2][2],2))))
+            # curr_roll = math.degrees(math.atan2(tag.pose_R[1][0],tag.pose_R[0][0]))
+
+            # Moving average distance and yaw reading
+            dist.append(manual_dist)
+            yaw.append(curr_yaw)
+
+            avg_yaw = round(np.average(yaw), 2)
+            avg_dist = round(np.average(dist), 2)
+
             #print("CALIB DIST", tag.pose_t[-1])
-            print("MANUAL DIST", manual_dist, " mm")
+            print(tag.tag_id)
+            print("MANUAL DIST", avg_dist, " mm")
+            print("YAW ", avg_yaw, " degree")
 
 
     if visualization:
