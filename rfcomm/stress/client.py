a5af8f48688fbb934641e816af765aecde385888
Tim-K3007 <oitsplazma444@gmail.com>
2024-01-21 07:43:36 -0500

    updated stress test

--- rfcomm/stress/client.py
+++ rfcomm/stress/client.py
@@ -1,55 +1,88 @@
 from enum import Enum
 import bluetooth
 import threading
-import time
+from time import sleep
 
+def run_test(socket):
+    sleep(15)
+    for i in range(6):
+        send_msg(socket, "server server_saying_hello_world")
+        for j in ["1", "2", "3", "4", "6"]:
+          send_msg(socket, "croc0" + j + " sending_message_hello_world")
+
+# list of bt mac addresses that will be running server
 class server_macs(Enum):
     jason = '28:D0:EA:60:BC:E6'
     tim = 'B0:DC:EF:86:98:0D'
 
+# lock so threads can share a variable 
 lock = threading.Lock()
+
+# shared variable so recieving thread can end when sending thread ends
+# false if sending thread has not ended and true if it has
 flag = False
 
-def find_server_mac():
-     possible_mac = [adr.value for adr in server_macs]
-     nearby_devices = bluetooth.discover_devices()
+# Finds which of the above listed macs are hosting the server and connect
+# returns the sockect object used to send messages
+# port is the port number that the server is bond to
+def find_and_connect(port=4):
+    possible_mac = [adr.value for adr in server_macs]
+    nearby_devices = bluetooth.discover_devices()
+    adr = None
      
-     for curr_adr in nearby_devices:
-          if curr_adr in possible_mac:
-               print("connecting to", server_macs(curr_adr).name)
-               return curr_adr 
-          
-     return None
+    for curr_adr in nearby_devices:
+        if curr_adr in possible_mac:
+            print("connecting to", server_macs(curr_adr).name)
+            adr = curr_adr
 
+    if adr is not None:
+        sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        for i in range(1, 4):
+            try:
+                sock.connect((adr, port))
+                if sock is not None:
+                    return sock
+                else:
+                    raise Exception("socket was none")
+            except Exception as e:
+                print("Failed to connect, try count:", i)
+                print(e)
+                sleep(1)
+        print("Failed to connect to server")
+        return None
+    else:
+        print("The server was not found")
+        return None
+
+# displays incoming messages
+# takes the socket that has the server connected
 def rec_msg(socket):
-     while True:
-        with lock:
-             if flag:
-                  break
-        print("waiting to recieve data")
+     while not flag:
         data = socket.recv(1024)
         if data:
             print(data)
 
-def run_test(socket):
-    time.sleep(15)
-    for i in range(6):
-        socket.send("server server_saying_hello_world")
-        for j in ["1", "2", "3", "4", "6"]:
-          socket.send("croc0" + j + " sending_message_hello_world")
+# sends given messages over bluetooth
+# takes the socket the server is connected to
+def send_msg(socket, text):
+     socket.send(text)
 
-s = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-s.connect((find_server_mac(), 4))
+if __name__ == '__main__':
+    # make sockect
+    s = find_and_connect(4)
+    
+    # create send and recieve thread
+    rec_thread = threading.Thread(target=rec_msg, args=(s,))
+    send_thread = threading.Thread(target=run_test, args=(s,))
 
-rec_thread = threading.Thread(target=rec_msg, args=(s,))
-send_thread = threading.Thread(target=run_test, args=(s,))
+    # start threads
+    rec_thread.start()
+    send_thread.start()
 
-rec_thread.start()
-send_thread.start()
+    # kill threads once they end
+    rec_thread.join()
+    send_thread.join()
 
-rec_thread.join()
-send_thread.join()
-
-print("Closing socket")
-
-s.close()
+    # close server
+    print("Closing socket")
+    s.close()
--- rfcomm/stress/server.py
+++ rfcomm/stress/server.py
@@ -3,21 +3,35 @@ import os
 from enum import Enum
 import threading
 
+def run_test():
+    for i in range(6):
+        send_msg("server server_saying_hello_world")
+        for j in ["1", "2", "3", "4", "6"]:
+            send_msg("croc0" + j + " sending_message_hello_world")
+
+# enum of all the crocs bt mac adress
 class croc_info(Enum):
-    croc00 = 0
+    croc00 = 'B8:27:EB:E3:F8:83'
     croc01 = 'B8:27:EB:71:7B:FB'
     croc02 = 'B8:27:EB:72:37:1B'
     croc03 = 'B8:27:EB:CA:AA:70'
     croc04 = 'B8:27:EB:CB:AB:FD'
-    croc05 = 5
+    croc05 = 'B8:27:EB:DC:84:5B'
     croc06 = 'B8:27:EB:10:4E:19'
 
+# lock so threads can used shared variables
 lock = threading.Lock()
-list_of_clients = {}
-client_threads = []
-close_croc_cnt = None
 
-def num_of_crocs():
+# dictionary relating croc names to client objects used to send messages
+list_of_clients = {}
+
+# the number of crocs that are connected
+connect_croc_cnt = None
+
+# sets up and connects crocs to server
+# port is the port that you want the server to be bound to
+def setup(port=4):
+    global connect_croc_cnt
     croc_macs = [adr.value for adr in croc_info]
     nearby_devices = bluetooth.discover_devices()
     cnt = 0
@@ -25,9 +39,32 @@ def num_of_crocs():
     for curr_adr in nearby_devices:
         if curr_adr in croc_macs:
             cnt = cnt + 1
-    
-    return cnt
 
+    # makes the server discoverable
+    os.system("bluetoothctl discoverable on")
+
+    socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    socket.bind(('', port))
+
+    connect_croc_cnt = cnt
+    print(connect_croc_cnt, "crocs detected")
+
+    socket.listen(1)
+    print("Ready to connect")
+
+    # connects to crocs and stores their client object in global dicitonary
+    for _ in range(connect_croc_cnt):
+        client, info = socket.accept()
+        print("Connecting to:", croc_info(info[0]).name)
+        list_of_clients[croc_info(info[0]).name] = client
+
+    print("All available crocs connected")
+    return socket
+
+# takes messages and sends them to correct croc
+# croc is the croc sending the message
+# data is an array of b'' with 0 being the croc target
+# and data[1] is the message being sent
 def handle_msg(croc, data):
     name = data[0].decode('ascii')
     if name == 'server':
@@ -37,8 +74,11 @@ def handle_msg(croc, data):
     else:
         print(data[0], "is not a connected croc\n")
 
+# takes messages from bt and uses handle message
+# croc is current croc recieveing the message
+# curr_client is the related client object with the current croc
 def rec_msg(croc, curr_client):
-    global close_croc_cnt
+    global connect_croc_cnt
     while True:
         data = curr_client.recv(1024).split(b' ')
         if data:
@@ -46,60 +86,39 @@ def rec_msg(croc, curr_client):
 
             if data[0] == b'quit':
                 with lock:
-                    close_croc_cnt -= 1
+                    connect_croc_cnt -= 1
                     list_of_clients.pop(croc)
                 print("Disconnected from", croc)
                 curr_client.close()
                 break
             
-            with lock:
-                handle_msg(croc=croc, data=data)
+            handle_msg(croc=croc, data=data)
 
+# sends messages by taking in inputs and using handle message function
 def send_msg(text):
-    print("ready to take input")
-    text = input().encode('ascii').split(b' ')
+    text = text.encode('ascii').split(b' ')
+    handle_msg('server', text)
 
-    with lock:
-        handle_msg('server', text)
+if __name__ == "__main__":
+    s = setup(4)
 
-def run_test():
-    for i in range(6):
-        send_msg("server server_saying_hello_world")
-        for j in ["1", "2", "3", "4", "6"]:
-            send_msg("croc0" + j, "sending_message_hello_world")
+    # starts the thread used for sending messages
+    send_thread = threading.Thread(target=run_test, args=())
+    send_thread.start()
 
-print
-os.system("bluetoothctl discoverable on")
+    # starts a thread for each client
+    client_threads = []
+    for croc, curr_client in list_of_clients.items():
+        curr_thread = threading.Thread(target=rec_msg, args=(croc, curr_client))
+        client_threads.append(curr_thread)
+        curr_thread.start()
 
-socket = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-socket.bind(('', 4))
+    # kill threads once they finish
+    send_thread.join()
+    for t in client_threads:
+        t.join()
 
-close_croc_cnt = num_of_crocs()
-print(close_croc_cnt, "crocs detected")
-
-socket.listen(1)
-print("Ready to connect")
-
-for _ in range(close_croc_cnt):
-    client, info = socket.accept()
-    print("Connecting to:", croc_info(info[0]).name)
-    list_of_clients[croc_info(info[0]).name] = client
-
-print("All available crocs connected")
-
-send_thread = threading.Thread(target=run_test, args=())
-send_thread.start()
-
-for croc, curr_client in list_of_clients.items():
-    curr_thread = threading.Thread(target=rec_msg, args=(croc, curr_client))
-    client_threads.append(curr_thread)
-    curr_thread.start()
-
-send_thread.join()
-
-for t in client_threads:
-    t.join()
-
-print("closing server")
-socket.close()
-os.system("bluetoothctl discoverable off")
+    # closes server and disables discoverable
+    print("closing server")
+    s.close()
+    os.system("bluetoothctl discoverable off")
