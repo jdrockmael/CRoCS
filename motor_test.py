31fd27bbddc02ab249ed83e2ab5303636302ea2b
jdrockmael <jdrockmael@wpi.edu>
2024-01-27 13:40:31 -0500

    motor code setup

--- motor_test.py
+++ motor_test.py
@@ -1,8 +1,34 @@
-from gpiozero import Motor
-import time
-motor1 = Motor(4, 14)
+#pin 35 - Motor FWD
+#pin 37 - Motor REV
+#2.5 = min, 12.5 = max
 
-motor1.forward()
-while 1:
-    time.sleep(5)
-    motor1.reverse()
+# Set up libraries and overall settings
+import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
+from time import sleep   # Imports sleep (aka wait or pause) into the program
+GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
+
+# Set up pin 16 for PWM
+GPIO.setup(35,GPIO.OUT)  # Sets up pin 16 to an output (instead of an input)
+GPIO.setup(37,GPIO.OUT)  # Sets up pin 16 to an output (instead of an input)
+
+motor_fwd = GPIO.PWM(35, 50)     # Sets up pin 16 as a PWM pin
+motor_rev = GPIO.PWM(37, 50)     # Sets up pin 16 as a PWM pin
+
+motor_fwd.start(0)               # Starts running PWM on the pin and sets it to 0
+motor_rev.start(0)               # Starts running PWM on the pin and sets it to 0
+
+
+# Move the motor back and forth
+motor_fwd.ChangeDutyCycle(2.5)     # Changes the pulse width to 3 (so moves the motor)
+sleep(1)                 # Wait 1 second
+motor_fwd.ChangeDutyCycle(12.5)
+sleep(1)
+motor_rev.ChangeDutyCycle(12.5)     # Changes the pulse width to 3 (so moves the motor)
+sleep(1)                 # Wait 1 second
+motor_rev.ChangeDutyCycle(2.5)
+sleep(1)
+
+# Clean up everything
+motor_fwd.stop()                 # At the end of the program, stop the PWM
+motor_rev.stop()                 # At the end of the program, stop the PWM
+GPIO.cleanup()           # Resets the GPIO pins back to defaults
