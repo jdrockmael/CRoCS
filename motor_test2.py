658dc402de9213f4c2d059608a8e625e1cd11f4b
jdrockmael <jdrockmael@wpi.edu>
2024-01-28 13:31:03 -0500

    everything working

--- motor_test2.py
+++ motor_test2.py
@@ -1,13 +1,13 @@
 from gpiozero import PhaseEnableMotor
-#import RPi.GPIO as GPIO
+#forward ccw, backward cw
 from time import sleep
 
 motor1 = PhaseEnableMotor(26, 13)
-motor2 = PhaseEnableMotor(6, 5)
+motor2 = PhaseEnableMotor(24, 23)
 
 for i in range(1,10):
     motor1.forward(i/10)
     sleep(2)
-    motor2.forward(i/10)
+    motor2.backward(i/10)
     sleep(2)
     i+=1
