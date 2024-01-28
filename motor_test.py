4f3d48fd58427f29978e0b7d174872f877346811
jdrockmael <jdrockmael@wpi.edu>
2024-01-28 13:41:57 -0500

    servo updated

--- motor_test.py
+++ motor_test.py
@@ -2,16 +2,16 @@ from gpiozero import PhaseEnableMotor, Servo
 #motor forward ccw, backward cw
 from time import sleep
 
-servo = Servo(7)
+servo = Servo(25)
 motor1 = PhaseEnableMotor(26, 13)
 motor2 = PhaseEnableMotor(24, 23)
 
 servo.min()
-sleep(0.5)
+sleep(1)
 servo.mid()
-sleep(0.5)
+sleep(1)
 servo.max()
-sleep(0.5)
+sleep(1)
 
 for i in range(1,10):
     motor1.forward(i/10)
