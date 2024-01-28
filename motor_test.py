438ac5ba9e8ab09145f1d893d9b1b5e02aff52c8
jdrockmael <jdrockmael@wpi.edu>
2024-01-28 13:36:54 -0500

    servo and motors

--- motor_test.py
+++ motor_test.py
@@ -1,34 +1,21 @@
-#pin 35 - Motor FWD
-#pin 37 - Motor REV
-#2.5 = min, 12.5 = max
+from gpiozero import PhaseEnableMotor, Servo
+#motor forward ccw, backward cw
+from time import sleep
 
-# Set up libraries and overall settings
-import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
-from time import sleep   # Imports sleep (aka wait or pause) into the program
-GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
+servo = Servo(7)
+motor1 = PhaseEnableMotor(26, 13)
+motor2 = PhaseEnableMotor(24, 23)
 
-# Set up pin 16 for PWM
-GPIO.setup(33,GPIO.OUT)  # Sets up pin 16 to an output (instead of an input)
-#GPIO.setup(37,GPIO.OUT)  # Sets up pin 16 to an output (instead of an input)
+servo.min()
+sleep(0.5)
+servo.mid()
+sleep(0.5)
+servo.max()
+sleep(0.5)
 
-motor_fwd = GPIO.PWM(33, 50)     # Sets up pin 16 as a PWM pin
-#motor_rev = GPIO.PWM(37, 50)     # Sets up pin 16 as a PWM pin
-motor_fwd.ChangeFrequency(1000)
-motor_fwd.start(7)               # Starts running PWM on the pin and sets it to 0
-#motor_rev.start(7.5)               # Starts running PWM on the pin and sets it to 0
-
-sleep(2)
-# Move the motor back and forth
-motor_fwd.ChangeDutyCycle(3)     # Changes the pulse width to 3 (so moves the motor)
-sleep(2)                 # Wait 1 second
-motor_fwd.ChangeDutyCycle(11)
-sleep(2)
-#motor_rev.ChangeDutyCycle(12.5)     # Changes the pulse width to 3 (so moves the motor)
-#sleep(2)                 # Wait 1 second
-#motor_rev.ChangeDutyCycle(2.5)
-#sleep(2)
-
-# Clean up everything
-motor_fwd.stop()                 # At the end of the program, stop the PWM
-#motor_rev.stop()                 # At the end of the program, stop the PWM
-GPIO.cleanup()           # Resets the GPIO pins back to defaults
+for i in range(1,10):
+    motor1.forward(i/10)
+    sleep(2)
+    motor2.backward(i/10)
+    sleep(2)
+    i+=1
