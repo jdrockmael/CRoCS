41c1a432f22eaabd8d4748c09b028403c5c1cc8f
jdrockmael <jdrockmael@wpi.edu>
2024-01-28 13:23:54 -0500

    speed testing

--- motor_test2.py
+++ motor_test2.py
@@ -5,8 +5,8 @@ from time import sleep
 servo = PhaseEnableMotor(26, 13)
 
 for i in range(10):
-    servo.forward(i)
+    servo.forward(i/10)
     sleep(1)
-    servo.backward(i)
+    servo.backward(i/10)
     sleep(1)
     i+=1
