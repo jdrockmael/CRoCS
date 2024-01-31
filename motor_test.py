d4a2266594661fb5b05cee3ca297b3e217aa56f0
jdrockmael <jdrockmael@wpi.edu>
2024-01-31 13:51:11 -0500

    all 3 motors running at once

--- motor_test.py
+++ motor_test.py
@@ -8,14 +8,8 @@ motor2 = PhaseEnableMotor(24, 23)
 
 servo.min()
 sleep(1)
-servo.mid()
-sleep(1)
-servo.max()
-sleep(1)
 
-for i in range(1,10):
-    motor1.forward(i/10)
-    sleep(2)
-    motor2.backward(i/10)
-    sleep(2)
-    i+=1
+servo.max()
+motor1.forward(0.5)
+motor2.backward(0.5)
+sleep(2)
