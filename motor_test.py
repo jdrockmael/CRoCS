2df86792f9c38e361cee19fd039659f1e6b66f94
jdrockmael <jdrockmael@wpi.edu>
2024-02-01 16:34:39 -0500

    ros wrapper written

--- motor_test.py
+++ motor_test.py
@@ -1,15 +1,43 @@
 from gpiozero import PhaseEnableMotor, Servo
 #motor forward ccw, backward cw
 from time import sleep
+import rospy
+from motor_test import ROSMotor
 
-servo = Servo(25)
-motor1 = PhaseEnableMotor(26, 13)
-motor2 = PhaseEnableMotor(24, 23)
+if __name__ == "__main__":
+    rospy.init_node("motor_driver")
+    motor = ROSMotor()
+    rospy.on_shutdown(motor.stop)
+    rospy.loginfo("Motor driver is now started, ready to get commands.")
+    rospy.spin()
 
-servo.min()
-sleep(1)
+class ROSMotor:
+    def __init__(self):
+        servo = Servo(25)
+        motor1 = PhaseEnableMotor(26, 13)
+        motor2 = PhaseEnableMotor(24, 23)
 
-servo.max()
-motor1.forward(0.5)
-motor2.backward(0.5)
-sleep(2)
+        servo.min()
+        sleep(1)
+
+    def forward(self, time):
+        self.motor1.forward(0.5)
+        self.motor2.backward(0.5)
+        sleep(time)
+
+    def backward(self, time):
+        self.motor1.backward(0.5)
+        self.motor2.forward(0.5)
+        sleep(time)
+    
+    def close_gripper(self):
+        self.servo.max()
+        sleep(1)
+
+    def open_gripper(self):
+        self.servo.min()
+        sleep(1)
+
+    def stop(self):
+        self.motor1.stop()
+        self.motor2.stop()
