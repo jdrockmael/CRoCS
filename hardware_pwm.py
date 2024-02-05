0bdbe11c9a9c7a2e81d3ceaae70aa7c08dbb8b14
jdrockmael <jdrockmael@wpi.edu>
2024-02-05 10:24:21 -0500

    hardware pwm setup

--- hardware_pwm.py
+++ hardware_pwm.py
@@ -1,9 +1,13 @@
 from gpiozero.pins.pigpio import PiGPIOFactory
 from gpiozero import Servo
+from time import sleep
 
-factory = PiGPIOFactory()
+factory = PiGPIOFactory('192.168.1.169')
 servo = Servo(25, pin_factory=factory)
 
 servo.min()
+sleep(1)
 servo.max()
+sleep(1)
 servo.mid()
+sleep(1)
