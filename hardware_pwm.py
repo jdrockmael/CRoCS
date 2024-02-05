6983525a1858e77fd641db787cef7a65157bc715
jdrockmael <jdrockmael@wpi.edu>
2024-02-05 11:00:20 -0500

    added blue motors

--- hardware_pwm.py
+++ hardware_pwm.py
@@ -1,36 +1,33 @@
-"""
-from gpiozero.pins.pigpio import PiGPIOFactory
-import pigpio
-from gpiozero import Servo
-from time import sleep
-
-pi = pigpio.pi()
-servo = pi.hardware_PWM(pi, 18, 800, 250000) # 800Hz 25% dutycycle
-
-factory = PiGPIOFactory('192.168.1.169')
-#servo = Servo(25, pin_factory=factory)
-
-servo.min()
-sleep(1)
-servo.max()
-sleep(1)
-servo.mid()
-sleep(1)
-"""
-
 import pigpio
 from time import sleep 
 
 servo = 18
+motor1 = 13
+motor2 = 23
  
 pwm = pigpio.pi() 
 pwm.set_mode(servo, pigpio.OUTPUT)
+pwm.set_mode(motor1, pigpio.OUTPUT)
+pwm.set_mode(motor2, pigpio.OUTPUT)
  
 pwm.set_PWM_frequency(servo, 50)
 pwm.set_PWM_range(servo, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
 
-pwm.hardware_PWM(servo, 50, 2000)
-sleep(10)
+pwm.set_PWM_frequency(motor1, 50)
+pwm.set_PWM_frequency(motor2, 50)
+pwm.set_PWM_range(motor1, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+pwm.set_PWM_range(motor2, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
 
-pwm.set_servo_pulsewidth(servo, 50)
+pwm.hardware_PWM(servo, 50, 2000)
+pwm.hardware_PWM(motor1, 50, 2000)
+pwm.hardware_PWM(motor2, 50, 2000)
 sleep(1)
+
+pwm.set_servo_pulsewidth(servo, 500)
+sleep(1)
+pwm.set_servo_pulsewidth(servo, 2500)
+sleep(1)
+
+pwm.set_PWM_dutycycle(motor1, 50)
+pwm.set_PWM_dutycycle(motor2, 50)
+sleep(3)
