7139890f773431698741c381b104a6ca59149311
jdrockmael <jdrockmael@wpi.edu>
2024-02-05 10:45:39 -0500

    set up for real

--- hardware_pwm.py
+++ hardware_pwm.py
@@ -1,9 +1,11 @@
+"""
 from gpiozero.pins.pigpio import PiGPIOFactory
 import pigpio
 from gpiozero import Servo
 from time import sleep
 
-servo = pigpio.pi.hardware_PWM(pigpio.pi, 18, 800, 250000) # 800Hz 25% dutycycle
+pi = pigpio.pi()
+servo = pi.hardware_PWM(pi, 18, 800, 250000) # 800Hz 25% dutycycle
 
 factory = PiGPIOFactory('192.168.1.169')
 #servo = Servo(25, pin_factory=factory)
@@ -14,3 +16,20 @@ servo.max()
 sleep(1)
 servo.mid()
 sleep(1)
+"""
+
+import pigpio
+import time
+
+servo = 18
+ 
+pwm = pigpio.pi() 
+pwm.set_mode(servo, pigpio.OUTPUT)
+ 
+pwm.set_PWM_frequency(servo, 50)
+pwm.set_PWM_range(servo, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+
+pwm.hardware_PWM(servo, 50, 2000)
+time.sleep(10)
+
+pwm.set_servo_pulsewidth(servo, 0)
