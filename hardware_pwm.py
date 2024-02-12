37495a26726731930a9f2b363b86d03e996b29e2
jdrockmael <jdrockmael@wpi.edu>
2024-02-12 10:23:40 -0500

    fixed encoder pins

--- hardware_pwm.py
+++ hardware_pwm.py
@@ -2,8 +2,8 @@ import pigpio
 from time import sleep 
 from gpiozero import RotaryEncoder
 
-encoder1 = RotaryEncoder(22, 17)
-encoder2 = RotaryEncoder(27, 16)
+encoder1 = RotaryEncoder(5, 6)
+encoder2 = RotaryEncoder(20, 21)
 
 servo = 18
 motor1 = 13
@@ -21,6 +21,7 @@ pwm.set_PWM_frequency(motor1, 50)
 pwm.set_PWM_frequency(motor2, 50)
 pwm.set_PWM_range(motor1, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
 pwm.set_PWM_range(motor2, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+sleep(1)
 
 pwm.set_servo_pulsewidth(servo, 500)
 sleep(1)
