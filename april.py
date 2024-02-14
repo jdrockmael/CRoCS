9884a74c405952ae57454107fb8fc5cacd732fcd
Dang Tran <dttran@wpi.edu>
2024-02-14 13:13:55 -0500

    Revamped ekf code and started combine ekf and april tag

---
+++ april.py
@@ -0,0 +1,171 @@
+import cv2
+from dt_apriltags import Detector
+import numpy as np
+import glob
+import math
+import collections
+import platform
+from filtering import ekf
+from time import time
+ardu = False
+if platform.node()[0:4] == "croc":
+    ardu = True
+
+cam_param = None
+if not ardu:
+    from cv2 import imshow
+
+    LINE_LENGTH = 5
+    CENTER_COLOR = (0, 255, 0)
+    CORNER_COLOR = (255, 0, 255)
+
+    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    objp = np.zeros((6*7,3), np.float32)
+    objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('calib_data/*.jpg')
+    
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, (7,6), None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners2)
+            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+            cam_param = ( mtx[0,0], mtx[1,1], mtx[0,2], mtx[1,2] )
+
+    ### Some utility functions to simplify drawing on the camera feed
+    # draw a crosshair
+    def plotPoint(image, center, color):
+        center = (int(center[0]), int(center[1]))
+        image = cv2.line(image,
+                        (center[0] - LINE_LENGTH, center[1]),
+                        (center[0] + LINE_LENGTH, center[1]),
+                        color,
+                        3)
+        image = cv2.line(image,
+                        (center[0], center[1] - LINE_LENGTH),
+                        (center[0], center[1] + LINE_LENGTH),
+                        color,
+                        3)
+        return image
+else:
+    # Arducam parameters
+    cam_param = (534.0708862263613, 534.1191479816413, 341.5340710724817, 232.94565221113476)
+
+# inches = np.array([10, 15, 18, 20])
+mm = np.array([200, 250, 300, 350, 400])
+#measurements = np.array([120, 142, 170, 186, 213]) # 6x6
+measurements = np.array([7.88, 9.45, 11.52, 14.1, 15.6]) #Alternate value# 6x6
+m, b = np.polyfit(measurements, mm, 1)
+
+# Convert april tag pose to real world measurements in mm
+def pose2real(measurement):
+    # Fit a linear model
+    return m * measurement + b
+
+yaw_l = collections.deque(maxlen=10)
+dist_l = collections.deque(maxlen=10)
+hor_dist_l = collections.deque(maxlen=10)
+range_l = collections.deque(maxlen=10)
+bearing_l = collections.deque(maxlen=10)
+
+cap = cv2.VideoCapture(0)
+at_detector = Detector()
+
+april_ekf = ekf.RobotEKF(95.25)
+u = np.array([0, 0])
+
+landmark = np.array([0, 190])
+
+t = time()
+while(1):
+    dt = time() - t
+    april_ekf.predict(u, dt)
+    t = time()
+    # Take each frame
+    _, frame = cap.read()
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    tags =at_detector.detect(grayscale, estimate_tag_pose=True, camera_params=cam_param, tag_size=5)
+
+    # color_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
+    
+    if tags:
+        # print(tags)
+        for tag in tags:
+            # print("tag_id: %s, center: %s" % (tag.tag_id, tag.center))
+            if not ardu: 
+                frame = plotPoint(frame, tag.center, CENTER_COLOR)
+            # width = []
+            # for idx in range(len(tag.corners)):
+            #     width.append(max(abs(tag.corners[idx-1, :] - tag.corners[idx,:])))
+            # width = np.average(width)
+
+            # Focal length = (Pixel width x distance) / Actual width
+            # Dist = (Actual Width x Focal Length) / Pixel Width
+            #print("FOCAL LENGTH: ", width * 60 / 5)
+            #these values work at 1.5inx1.5in 360 and 60
+            # focal = 360
+            # manual_dist = focal * 60 / width
+            # curr_roll = math.degrees(math.atan2(tag.pose_R[1][0],tag.pose_R[0][0]))
+
+            center_hor = 0 # real world distance of half the length april tag side in mm
+            if tag.tag_id == 0:
+                center_hor = 55.5
+            elif tag.tag_id == 1:
+                center_hor = 10
+            elif tag.tag_id == 2:
+                center_hor = 12
+            elif tag.tag_id == 3:
+                center_hor = 15
+            else:
+                center_hor = 60.5
+                
+            yaw = math.degrees(math.atan2(-tag.pose_R[2][0],math.sqrt(pow(tag.pose_R[2][1],2) + pow(tag.pose_R[2][2],2))))
+            distance = pose2real(tag.pose_t[2])      
+            hor_distance = pose2real(tag.pose_t[0]) # 25.4mm is the real world distance from center of april tag to edge
+            hyp = math.sqrt(distance**2 + hor_distance**2)
+            bearing = math.degrees(math.atan2(hor_distance, distance))
+
+            # Moving average distance and yaw reading
+            yaw_l.append(yaw)
+            dist_l.append(distance)
+            hor_dist_l.append(hor_distance)
+            range_l.append(hyp)
+            bearing_l.append(bearing)
+
+            # avg_yaw = round(np.average(yaw_l), 2)
+            avg_dist = round(np.average(dist_l), 2)
+            avg_hor_dist = round(np.average(hor_dist_l), 2)
+            avg_range = round(np.average(range_l), 2)
+            avg_bearing = round(np.average(bearing_l), 2)
+
+            # print("ID ",tag.tag_id)
+            # print("YAW ", avg_yaw, " degree")
+            # print("Distance ", avg_dist, " mm")
+            # print("Horizontal distance", avg_hor_dist, " mm")
+            # print("Range ", avg_range, " mm")
+            # print("Bearing ", avg_bearing, " degree")
+
+            z = np.array([[avg_range],
+                [np.deg2rad(avg_bearing) ]])
+            april_ekf.update(z, landmark, dt)
+
+            print(april_ekf.pose)
+
+
+
+    # Disable visualization for running on server
+    if not ardu:
+        cv2.imshow('Detected tags', frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cv2.destroyAllWindows()
--- filtering/ekf.py
+++ filtering/ekf.py
@@ -11,204 +11,226 @@ from numpy.random import randn
 from IPython.display import display
 sympy.init_printing(use_latex='mathjax')
 
-# Function for h 
-def Hx(x, april_pose, v):
-    """ takes a state variable and returns the measurement
-    that would correspond to that state.
-    """
-    xk, yk, thetak = x[0, 0], x[1, 0], x[2, 0]
-    x2, y2= april_pose[0], april_pose[1]
+VAR_ENC = 1e-1
+VAR_RANGE = 0 
+VAR_BEARING = 0
+VAR_POSE1 = 1e-2
+VAR_POSE2 = 1e-2
 
-    vxk, vx2, vyk, vy2, vtheta = v[0], v[1], v[2], v[3], v[4]
-    xk = xk + vxk
-    yk = yk + vyk
-    x2 = x2 + vx2
-    y2 = y2 + vy2
-    thetak = thetak + vtheta
-    
-    Hx = np.array([[math.sqrt((y2 + vy2 - yk - vyk)**2 + (x2 + vx2 -xk - vxk)**2)],
-                [thetak + vtheta - math.atan2((y2 + vy2 -yk - vyk),(x2 + vx2-xk - vxk))]])
-    return Hx
-
-def z_landmark(lmark, sim_pos, std_range, std_bearing, v):
-    xk, yk, thetak = sim_pos[0,0], sim_pos[1, 0], sim_pos[2, 0]
-    x2, y2= lmark[0], lmark[1]
-
-    vxk, vx2, vyk, vy2, vtheta = v[0], v[1], v[2], v[3], v[4]
-    xk = xk + vxk
-    yk = yk + vyk
-    x2 = x2 + vx2
-    y2 = y2 + vy2
-    thetak = thetak + vtheta
-
-    rng = math.sqrt((y2 + vy2 - yk - vyk)**2 + (x2 + vx2 -xk - vxk)**2)
-    bearing = thetak + vtheta - math.atan2((y2 + vy2 -yk - vyk),(x2 + vx2-xk - vxk))
-    z = np.array([[rng +  randn() * std_range],
-                [bearing + randn() * std_bearing]])
-    return z
-
-# Function for H matrix, jacobian of h w.r.t pose
-def H_of(x, april_pose, v):
-    """ compute Jacobian of H matrix where h(x) computes 
-    the range and bearing to a landmark for state x """
-
-    xk, yk, thetak = x[0, 0], x[1, 0], x[2, 0]
-    x2, y2 = april_pose[0], april_pose[1]
-
-    vxk, vx2, vyk, vy2, vtheta = v[0], v[1], v[2], v[3], v[4]
-    xk = xk + vxk
-    yk = yk + vyk
-    x2 = x2 + vx2
-    y2 = y2 + vy2
-    thetak = thetak + vtheta
-
-    H = np.array([[-((x2-xk)/math.sqrt((y2-yk)**2 + (x2-xk)**2)), -((y2-yk)/math.sqrt((y2-yk)**2 + (x2-xk)**2)), 0],
-                [-(y2-yk)/((y2-yk)**2 + (x2-xk)**2), -(x2-xk)/((y2-yk)**2 + (x2-xk)**2), 1]])
-    return H
-
-def residual(a, b):
-    """ compute residual (a-b) between measurements containing 
-    [xc, yc, phi]. Yaw is normalized to [-pi, pi)"""
-    y = a - b
-    y[1] = y[1] % (2 * np.pi)    # force in range [0, 2 pi)
-    if y[1] > np.pi:             # move to [-pi, pi)
-        y[1] -= 2 * np.pi
-    return y
-
-class RobotEKF(EKF):
-    def __init__(self, dt, wheelbase, std_WL, std_WR):
-        EKF.__init__(self, 3, 2, 2)
-        self.dim_z = 2
+class RobotEKF():
+    def __init__(self, wheelbase):        
+        # EKF.__init__(self, 3, 2, 2)
+        # Dimensions of pose, measurement, and control vectors
         self.dim_x = 3
-        self.dt = dt
+        self.pose = np.zeros((self.dim_x, 1))   # state
+        self.dim_z = 2
+        self.dim_u = 2
         self.wheelbase = wheelbase
-        self.std_WL = std_WL
-        self.std_WR = std_WR
 
-        x, y, theta, Vl, Vr, t, x2, y2, b = sympy.symbols('x, y, theta, Vl, Vr, t, x2, y2, b')
-        wr, wl = sympy.symbols('wr, wl')
-        vxk, vx2, vyk, vy2, vtheta = sympy.symbols('vxk, vx2, vyk, vy2, vtheta')
+        self.P = np.eye(self.dim_x)                           # uncertainty covariance
+        self.P[0,0] = 1e-2
+        self.P[1,1] = 1e-2
+        self.Q = np.diag([VAR_ENC, VAR_ENC])                  # process uncertainty
+        self.R = np.diag([VAR_POSE1, VAR_POSE1, VAR_POSE1, VAR_POSE2, VAR_POSE2])
 
+        # Initializing variables
+        x, y, theta, Vl, Vr, dt, x2, y2, b = symbols('x, y, theta, Vl, Vr, dt, x2, y2, b')
+        wr, wl = symbols('wr, wl')
+        vxk, vx2, vyk, vy2, vthetak = symbols('vxk, vx2, vyk, vy2, vthetak')
+
+        # f matrix estimate the next state
         self.f = Matrix(
-            [[x + t*(Vr + wr + Vl + wl)/2 * sympy.cos(theta)],
-            [y + t*(Vr + wr + Vl + wl)/2 * sympy.sin(theta)],
-            [theta + t*(Vr + wr - (Vl + wl))/b]]
+            [[x + dt*(Vr + wr + Vl + wl)/2 * sympy.cos(theta)],
+            [y + dt*(Vr + wr + Vl + wl)/2 * sympy.sin(theta)],
+            [theta + dt*(Vr + wr - (Vl + wl))/b]]
         )
 
+        # h matrix convert pose to measurement
         self.h = Matrix(
             [[sympy.sqrt((y2+vy2-y-vyk)**2+(x2+vx2-x-vxk)**2)],
-             [theta - sympy.atan2(y2+vyk-y-vyk,x2+vx2-x-vxk)]]
+             [theta + vthetak - sympy.atan2(y2+vyk-y-vyk,x2+vx2-x-vxk)]]
         )
 
-        process_noise = Matrix([wl, wr])
-        measurement_noise = ([vxk, vx2, vyk, vy2, vtheta])
         pose = Matrix([x, y, theta])
+        process_noise = Matrix([wl, wr])
+        measurement_noise = Matrix([vxk, vyk, vthetak, vx2, vy2])
 
         self.H_j = self.h.jacobian(pose)
         self.V_j = self.h.jacobian(measurement_noise)
         self.A_j = self.f.jacobian(pose)
         self.W_j= self.f.jacobian(process_noise)
 
-        self.subs = {x:0, y: 0, theta:0, t:self.dt, Vl:0, Vr:0, b:self.wheelbase, 
-                     wl: 0, wr:0, vxk: 0, vx2: 0, vyk: 0, vy2: 0, vtheta: 0, x2:0, y2:0}
+        self.subs = {x:0, y: 0, theta:0, Vl:0, Vr:0, dt:0, x2:0, y2:0, b:wheelbase, 
+                     wl: 0, wr:0, 
+                     vxk: 0, vx2: 0, vyk: 0, vy2: 0, vthetak: 0}
+
+        self.dt = dt
         self.x_x, self.x_y, self.x_theta = x, y, theta
         self.x2_x, self.x2_y = x2, y2
-        self.Vl, self.Vr = Vl, Vr
-        self.Wl, self.Wr = wl, wr
-        self.vxk, self.vx2, self.vyk, self.vy2, self.vtheta = vxk, vx2, vyk, vy2, vtheta
+        self.vl, self.vr = Vl, Vr
+        self.w_Wl, self.w_Wr = wl, wr
+        self.V_vxk, self.V_vx2, self.V_vyk, self.V_vy2, self.V_vthetak = vxk, vx2, vyk, vy2, vthetak
+
+    def predict(self, u, dt):
+        """ Prediction step of EKF.
+        x = f(x,u,w=0)
+        P = A @ P @ A' + W @ Q @ W'
+        :u: 2x1 vector of left and right wheel speed in mm/s
+        :dt: time elapsed
+        """
     
-    def predict(self, u, w):
-        self.x = self.move(self.x, u, self.dt, w)
-        self.subs[self.x_x] = self.x[0,0]
-        self.subs[self.x_y] = self.x[1,0]
-        self.subs[self.x_theta] = self.x[2,0]
+        self.subs[self.dt] = dt
 
-        self.subs[self.Vl] = u[0]
-        self.subs[self.Vr] = u[1]
+        self.subs[self.x_x] = self.pose[0,0]
+        self.subs[self.x_y] = self.pose[1,0]
+        self.subs[self.x_theta] = self.pose[2,0]
 
-        self.subs[self.Wl] = w[0]
-        self.subs[self.Wr] = w[1]
+        self.subs[self.vl] = u[0]
+        self.subs[self.vr] = u[1]
+
+        self.subs[self.w_Wl] = 0
+        self.subs[self.w_Wr] = 0
+
+        # Update pose
+        self.pose = np.array(self.f.evalf(subs=self.subs)).astype(float)  
+
+        # Set the process noise
+        w = np.random.normal(0, self.Q)
+        self.subs[self.w_Wl] = w[0,0]
+        self.subs[self.w_Wr] = w[1,1]
 
         A = np.array(self.A_j.evalf(subs=self.subs)).astype(float)        
         W = np.array(self.W_j.evalf(subs=self.subs)).astype(float)
 
-        Q = np.array([[self.std_WL**2, 0],
-                      [0, self.std_WR**2]])
+        # Update covariance
+        self.P = A @ self.P @ A.T + W @ self.Q @ W.T
+
+    def update(self, z, pose2, dt):
+        """ Update/Correction step of EKF.
+        K = PH'(HPH' + VRV')^-1
+        x = x + K(z - h(x,0))
+        P = (I - KH)P
+        :z: 2x1 measurement vector of range and bearing
+        :pose2: 2x1 vector consisting of x2 and y2
+        :dt: elapsed time
+        """
+        # Set poses
+        self.subs[self.x_x] = self.pose[0,0]
+        self.subs[self.x_y] = self.pose[1,0]
+        self.subs[self.x_theta] = self.pose[2,0]
+
+        self.subs[self.x2_x] = pose2[0]
+        self.subs[self.x2_y] = pose2[1]
+
+        self.subs[self.dt] = dt
+
+        # Set the process noise
+        v = np.random.normal(0, self.R)
+        self.subs[self.V_vxk] = v[0,0]
+        self.subs[self.V_vyk] = v[1,1]
+        self.subs[self.V_vthetak] = v[2,2]
+        self.subs[self.V_vx2] = v[3,3]
+        self.subs[self.V_vy2] = v[4,4]
+
+        H = np.array(self.H_j.evalf(subs=self.subs)).astype(float) 
+        V = np.array(self.V_j.evalf(subs=self.subs)).astype(float) 
+        K = self.P @ H.T @ np.linalg.inv(H @ self.P @ H.T + V @ self.R @ V.T)
+        # print("K: ", K)
+        self.pose += K @ self.residual(z, pose2)
         
-        self.P = A @ self.P @ A.T + W @ Q @ W.T
+        # Update covariance, equivalent to P = (I - KH)P
+        self.P -= K @ H @ self.P
 
-    def move(self, x, u, dt, w):
-        vl, vr = u[0], u[1]
-        wl, wr = w[0], w[1]
-        theta = x[2,0]
+    def residual(self, z, pose2):
+        """ Helper function to convert pose to measurement with h matrix
+        """
+        # Set pose
+        self.subs[self.x_x] = self.pose[0,0]
+        self.subs[self.x_y] = self.pose[1,0]
+        self.subs[self.x_theta] = self.pose[2,0]
 
-        dx = np.array([[dt*(vr + wr + vl + wl)/2 * cos(theta)],
-                       [dt*(vr + wr + vl + wl)/2 * sin(theta)],
-                       [dt*(vr + wr - (vl + wl))/self.wheelbase]])
-        return x + dx 
-     
+        self.subs[self.x2_x] = pose2[0]
+        self.subs[self.x2_y] = pose2[1]
+
+        # Set the process noise
+        self.subs[self.V_vxk] = 0
+        self.subs[self.V_vyk] = 0
+        self.subs[self.V_vthetak] = 0
+        self.subs[self.V_vx2] = 0
+        self.subs[self.V_vy2] = 0
+
+        # print("Pose2: ", pose2)
+        # print("Pose: ", self.pose)
+
+        innovation = np.array(self.h.evalf(subs=self.subs)).astype(float) 
+        
+        # print("Innovation: ", innovation)
+        zx = z - innovation
+
+        # Normalize bearing to [-pi, pi]
+        zx[-1] = zx[-1] % (2 * np.pi)
+        if zx[-1] > np.pi:             
+            zx[-1] -= 2 * np.pi
+
+        # print("Zx: ", zx)
+
+        return zx
     
-def ekf_update(ekf, z, landmark, v):
-    ekf.update(z, HJacobian=H_of, Hx=Hx,residual=residual,args=(landmark, v), hx_args=(landmark, v))
+    def z_landmark(self, pose2):
+        """ Helper function to convert pose to measurement with h matrix
+        """
+        # Set pose
+        self.subs[self.x_x] = self.pose[0,0]
+        self.subs[self.x_y] = self.pose[1,0]
+        self.subs[self.x_theta] = self.pose[2,0]
 
+        self.subs[self.x2_x] = pose2[0]
+        self.subs[self.x2_y] = pose2[1]
+
+        # Set the process noise
+        self.subs[self.V_vxk] = 0
+        self.subs[self.V_vyk] = 0
+        self.subs[self.V_vthetak] = 0
+        self.subs[self.V_vx2] = 0
+        self.subs[self.V_vy2] = 0
+
+        return np.array(self.h.evalf(subs=self.subs)).astype(float) 
+
+ekf = RobotEKF(95.25)
+
+landmarks = np.array([[60, -20], [20, 30], [40, 20]])
 dt = 0.1
 
-def run_localization(landmarks, std_WL, std_WR, std_range, std_bearing, step=1, ellipse_step=10, v_m=[0,0,0,0,0], w_m=[0,0], ylim=None):
-    ekf = RobotEKF(dt, wheelbase=0.5, std_WL=std_WL, std_WR=std_WR)
-    ekf.x = np.array([[2, 6, 0]]).T
-    ekf.P = np.diag([0, 0.1, 0.1])
-    ekf.R = np.diag([std_range**2, std_bearing**2])
-    
-    v = v_m
-    
-    sim_pos = ekf.x.copy()
-    u = np.array([1, 1.01])
+u = np.array([5, 12])
 
-    plt.figure()
+plt.figure()
+if landmarks.size != 0:
     plt.scatter(landmarks[:, 0], landmarks[:, 1], marker='s', s=60)
 
-    track = []
-    for i in range(200):
-        sim_pos = ekf.move(sim_pos, u, dt, w=w_m) # simulate robot
-        track.append(sim_pos)
+track = []
+for i in range(100):
+    ekf.predict(u, dt)
+    sim_pos = ekf.pose.copy()
+    track.append(sim_pos)
 
-        if i % step == 0:
-            ekf.predict(u=u, w=w_m)
+    if i % 10 == 0:
+        plot_covariance_ellipse(
+            (ekf.pose[0,0], ekf.pose[1,0]), ekf.P[0:2, 0:2], 
+                std=15, facecolor='k', alpha=0.3)
 
-            if i % ellipse_step == 0:
-                plot_covariance_ellipse(
-                    (ekf.x[0,0], ekf.x[1,0]), ekf.P[0:2, 0:2], 
-                     std=6, facecolor='k', alpha=0.3)
+    for lmark in landmarks:
+        z = ekf.z_landmark(lmark)
+        ekf.update(z, lmark, dt)
 
-            for lmark in landmarks:
-                z = z_landmark(lmark, sim_pos, std_range, std_bearing, v)
-                ekf_update(ekf, z, lmark, v)
-
-            if i % ellipse_step == 0:
-                plot_covariance_ellipse(
-                    (ekf.x[0,0], ekf.x[1,0]), ekf.P[0:2, 0:2],
-                    std=6, facecolor='g', alpha=0.8)
-                
-    track = np.array(track)
-    plt.plot(track[:, 0], track[:,1], color='k', lw=2)
-    plt.axis('equal')
-    plt.title("EKF Robot localization")
-    # plt.ylim([0, 30])
-    if ylim is not None: plt.ylim(*ylim)
-    plt.show()
-    return ekf
-
-# landmarks = np.array([[4, 10], [10, 17], [15, 15]])
-# landmarks = np.array([[4, 10], [10, 17]])
-
-landmarks = np.array([[4, 10]])
-# landmarks = np.array([])
-
-# Noise/weight for Vl and Vr in update step
-w = [0.1, 0.1]
-# Noise/weight for xk, x2, yk, y2, thetak in correction step
-v = [0.1, 0.1, 0.1, 0.1, 0.1] 
-
-
-ekf = run_localization(landmarks, std_WL=0.1, std_WR=0.1, std_range=0.2, std_bearing=0.1, v_m=v, w_m=w)
-print(ekf.x)
+    if i % 10 == 0:
+        plot_covariance_ellipse(
+            (ekf.pose[0,0], ekf.pose[1,0]), ekf.P[0:2, 0:2],
+            std=15, facecolor='g', alpha=0.8)
+            
+track = np.array(track)
+plt.plot(track[:, 0], track[:,1], color='k', lw=2)
+plt.axis('equal')
+plt.title("EKF Robot localization")
+# plt.ylim([-100, 100])
+# plt.xlim([-5, 80])
+plt.show()
