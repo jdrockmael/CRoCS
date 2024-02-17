2d4ca8bda15f4d825c7702e954c22f8619849910
jdrockmael <jdrockmael@wpi.edu>
2024-02-17 12:25:35 -0500

    reverting to software pwm

--- motor_test2.py
+++ motor_test2.py
@@ -1,9 +1,12 @@
-from gpiozero import PhaseEnableMotor
+from gpiozero import PhaseEnableMotor, RotaryEncoder
 #forward ccw, backward cw
 from time import sleep
 
 motor1 = PhaseEnableMotor(26, 13)
 motor2 = PhaseEnableMotor(24, 23)
+
+encoder1 = RotaryEncoder(5, 6)
+encoder2 = RotaryEncoder(20, 21)
 
 for i in range(1,10):
     motor1.forward(i/10)
@@ -11,3 +14,5 @@ for i in range(1,10):
     motor2.backward(i/10)
     sleep(2)
     i+=1
+    print(encoder1.value)
+    print(encoder2.value)
