61316021ad7197ce0a711639cd773f70f6b7afde
jdrockmael <jdrockmael@wpi.edu>
2024-02-18 14:03:07 -0500

    encoders working

--- motor_test2.py
+++ motor_test2.py
@@ -1,16 +1,44 @@
 from gpiozero import Device, PhaseEnableMotor, RotaryEncoder
-from gpiozero.pins.native import NativeFactory
+from gpiozero.pins.pigpio import PiGPIOFactory
 #forward ccw, backward cw
 from time import sleep
 
-Device.pin_factory = NativeFactory()
+Device.pin_factory = PiGPIOFactory()
 
-#motor1 = PhaseEnableMotor(26, 13)
-#motor2 = PhaseEnableMotor(24, 23)
+motor1 = PhaseEnableMotor(24, 23)
+motor2 = PhaseEnableMotor(21, 20)
 
-encoder1 = RotaryEncoder(5, 6)
-encoder2 = RotaryEncoder(20, 21)
+motor1.stop()
+motor2.stop()
+sleep(1)
 
-while(True):
-    print(encoder1.value)
-    print(encoder2.value)
+encoder1 = RotaryEncoder(6, 5, max_steps = 256000)
+encoder2 = RotaryEncoder(27, 22, max_steps = 256000)
+
+motor1.forward(0.5)
+motor2.backward(0.5)
+sleep(0.01)
+
+i = 0
+while(i < 5000):
+    print("1: ", encoder1.steps)
+    print("2: ", encoder2.steps)
+    sleep(0.0001)
+    i+=1
+
+motor1.backward(0.5)
+motor2.forward(0.5)
+sleep(0.01)
+
+j = 0
+while(j < 5000):
+    print("1: ", encoder1.steps)
+    print("2: ", encoder2.steps)
+    sleep(0.0001)
+    j+=1
+
+sleep(1)
+motor1.stop()
+motor2.stop()
+
+
