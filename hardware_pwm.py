4d4439509552db05001e2850da574937309957bf
jdrockmael <jdrockmael@wpi.edu>
2024-02-18 12:13:58 -0500

    fixed freq and duty cycle

--- hardware_pwm.py
+++ hardware_pwm.py
@@ -17,20 +17,20 @@ pwm.set_mode(servo, pigpio.OUTPUT)
 pwm.set_mode(motor1, pigpio.OUTPUT)
 pwm.set_mode(motor2, pigpio.OUTPUT)
  
-pwm.set_PWM_frequency(servo, 500)
-pwm.set_PWM_range(servo, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+pwm.set_PWM_frequency(servo, 50)
+pwm.set_PWM_range(servo, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
 
-pwm.set_PWM_frequency(motor1, 500)
-pwm.set_PWM_frequency(motor2, 500)
-pwm.set_PWM_range(motor1, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
-pwm.set_PWM_range(motor2, 2000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
-pwm.hardware_PWM(motor1, 500, 1000)
-pwm.hardware_PWM(motor2, 500, 1000)
+pwm.set_PWM_frequency(motor1, 50)
+pwm.set_PWM_frequency(motor2, 50)
+pwm.set_PWM_range(motor1, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+pwm.set_PWM_range(motor2, 20000) # 1,000,000 / 50 = 20,000us for 100% duty cycle
+pwm.hardware_PWM(motor1, 50, 10000)
+pwm.hardware_PWM(motor2, 50, 10000)
 sleep(1)
 
 pwm.set_servo_pulsewidth(servo, 500)
 sleep(1)
-pwm.set_servo_pulsewidth(servo, 2000)
+pwm.set_servo_pulsewidth(servo, 2500)
 sleep(1)
 
 pwm.set_PWM_dutycycle(motor1, 50)
