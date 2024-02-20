57d6b322150e503a45d6b835c35ed389df9fd484
Dangalang25 <dttran@wpi.edu>
2024-02-20 18:29:45 +0000

    Enable detect AT of multiple sizes and remove pose2real

--- april.py
+++ april.py
@@ -5,6 +5,7 @@ import math
 import collections
 import rospy
 from std_msgs.msg import Float32MultiArray
+import time
 
 class AprilCam():
     def __init__(self):
@@ -12,33 +13,30 @@ class AprilCam():
         self.cap = cv2.VideoCapture(0)
         self.at_detector = Detector()
 
-        mm = np.array([200, 250, 300, 350, 400])
-
-        pixel6x6 = np.array([7.88, 9.45, 11.52, 14.1, 15.6]) # Cam pixel readings of 6x6
-        self.m, self.b = {}, {}
-        self.m[6], self.b[6] = np.polyfit(pixel6x6, mm, 1)
-
         self.range_l = collections.deque(maxlen=10)
         self.bearing_l = collections.deque(maxlen=10)
-
-    # Convert april tag pose to real world measurements in mm
-    def pose2real(self, measurement, length):
-        # Fit a linear model
-        return self.m[length] * measurement + self.b[length]
-
+       
+        
+        #self.start = time.time()
     def get_measurements(self):
         # Take each frame
         _, frame = self.cap.read()
         grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        tags =self.at_detector.detect(grayscale, estimate_tag_pose=True, camera_params=self.cam_param, tag_size=5)
+
+        tags_side =self.at_detector.detect(grayscale, estimate_tag_pose=True, camera_params=self.cam_param, tag_size=0.02)
+
+        #tags_face = self.at_detector.detect(grayscale, estimate_tag_pose=True, camera_params=self.cam_param, tag_size=0.122)
 
         measurement = []
-        
-        if tags:
-            for tag in tags:                    
-                distance = self.pose2real(tag.pose_t[2], 6)      
-                hor_distance = self.pose2real(tag.pose_t[0], 6) # 25.4mm is the real world distance from center of april tag to edge
+        if tags_side:
+            for i, tag_side in enumerate(tags_side):
+                tag = tag_side
+                if tag_side.tag_id == 0:
+                    #tag = tags_face[i]
+                    tag = self.at_detector.detect(grayscale, estimate_tag_pose=True, camera_params=self.cam_param, tag_size=0.122)[i]
+                distance = tag.pose_t[2]
+                hor_distance = tag.pose_t[0] 
                 range = math.sqrt(distance**2 + hor_distance**2)
                 bearing = math.degrees(math.atan2(hor_distance, distance))
 
@@ -49,8 +47,6 @@ class AprilCam():
                 avg_range = round(np.average(self.range_l), 2)
                 avg_bearing = round(np.average(self.bearing_l), 2)
 
-                print(tag.tag_id)
-                print(avg_range)
                 measurement.append(Float32MultiArray(data=[float(tag.tag_id), float(avg_range), float(avg_bearing), float(2)]))
                 # return {
                 #     "ID": tag.tag_id,
@@ -58,7 +54,9 @@ class AprilCam():
                 #     "Bearing": avg_bearing, # Degrees
                 #     "AprilID": 2
                 # }
-
+        #self.end = time.time()
+        #print("Elapased time: ", self.end - self.start)
+        #self.start = time.time()
         return measurement
 
     def stop(self):
