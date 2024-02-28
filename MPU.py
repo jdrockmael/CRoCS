810ab5d66fadee88e07ec1c4aa26643fcb3bfdcc
Benji002 <Benji202002@gmail.com>
2024-02-28 12:11:03 -0500

    Update MPU.py

--- MPU.py
+++ MPU.py
@@ -50,7 +50,7 @@ def read_raw_data(addr):
         return value
 
 
-bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+bus = SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
 Device_Address = 0x68   # MPU6050 device address
 
 MPU_Init()
