eacb3d330ecc650e8e5a89a579a029524f16a034
Tim-K3007 <oitsplazma444@gmail.com>
2024-03-17 13:47:17 -0400

    gonna start the ik test

--- scripts/ik.py
+++ scripts/ik.py
@@ -2,31 +2,31 @@
 import rospy
 from std_msgs.msg import Float32MultiArray
 from time import sleep
-from drivers.motor_dr import motor
 from math import atan2, sqrt
 
 curr_pose = None
+eff_pub = rospy.Publisher("wheel_effort", Float32MultiArray, queue_size=1)
 
-def control_loop(cam : Float32MultiArray):
-    data = cam.data
-    delta_t = 0.001
+# def control_loop(cam : Float32MultiArray):
+#     data = cam.data
+#     delta_t = 0.001
 
-    if len(data) != 0:
-        heading_err = data[2] - 0.0
-        distance_err = data[1] - 0.2 # in meters
+#     if len(data) != 0:
+#         heading_err = data[2] - 0.0
+#         distance_err = data[1] - 0.2 # in meters
         
-        linear = distance_err * 5
-        angular_l = heading_err * 1
-        angular_r = -heading_err * 1
+#         linear = distance_err * 5
+#         angular_l = heading_err * 1
+#         angular_r = -heading_err * 1
 
-        l_eff = linear + angular_l
-        r_eff = linear + angular_r
+#         l_eff = linear + angular_l
+#         r_eff = linear + angular_r
 
-        motor.drive(l_eff, r_eff)
+#         motor.drive(l_eff, r_eff)
 
-        sleep(delta_t)
-    else:
-        motor.stop()
+#         sleep(delta_t)
+#     else:
+#         motor.stop()
 
 def update_pose(pose : Float32MultiArray):
     global curr_pose
@@ -46,11 +46,11 @@ def drive_to(pose):
         l_eff = linear + angular_l
         r_eff = linear + angular_r
 
-        motor.drive(l_eff, r_eff)
+        eff_pub.publish(Float32MultiArray(data=[l_eff, r_eff]))
 
         sleep(0.001)
 
-    motor.stop()
+    eff_pub.publish(Float32MultiArray(data=[0, 0]))
 
 def turn_to(heading):
     desired_heading = heading
@@ -63,12 +63,11 @@ def turn_to(heading):
         l_eff = angular_l
         r_eff = angular_r
 
-        motor.drive(l_eff, r_eff)
+        eff_pub.publish(Float32MultiArray(data=[l_eff, r_eff]))
 
-        # not sure if I need the sleep or not but we can test
-        #sleep(0.001)
+        sleep(0.001)
     
-    motor.stop()
+    eff_pub.publish(Float32MultiArray(data=[0, 0]))
 
 def drive_to_point(desired : Float32MultiArray):
     desired_pose = desired.data
@@ -81,4 +80,5 @@ if __name__ == '__main__':
     rospy.init_node('ik')
     #rospy.Subscriber("range", Float32MultiArray, control_loop)
     rospy.Subscriber("curr_pose", Float32MultiArray, update_pose)
+    rospy.Subscriber("target_pose", Float32MultiArray, drive_to_point)
     rospy.spin()
--- scripts/motor_test2.py
+++ scripts/motor_test2.py
@@ -2,10 +2,9 @@ import rospy
 from std_msgs.msg import Float32MultiArray
 from time import sleep
 
-pub = rospy.Publisher("wheel_effort", Float32MultiArray, queue_size=1)
+#pub = rospy.Publisher("wheel_effort", Float32MultiArray, queue_size=1)
+pub = rospy.Publisher("target_pose", Float32MultiArray, queue_size=1)
 
 if __name__ == '__main__':
     rospy.init_node('motor_test')
-    pub.publish(Float32MultiArray(data=[0.3, 0.3]))
-    sleep(1)
-    pub.publish(Float32MultiArray(data=[0.0, 0.0]))
+    pub.publish(Float32MultiArray(data=[0.1, 0.2, 3.14]))
