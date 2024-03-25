7f149c0f949c87fcd66e9665fa9a5eae7735c6e8
Tim-K3007 <oitsplazma444@gmail.com>
2024-03-25 10:42:46 -0400

    testing threaded speed controller

--- scripts/locomotion.py
+++ scripts/locomotion.py
@@ -5,20 +5,15 @@ from std_msgs.msg import Float32MultiArray
 from gpiozero import Device, PhaseEnableMotor, RotaryEncoder
 from gpiozero.pins.pigpio import PiGPIOFactory
 from math import pi
-from threading import Lock, Thread
+from threading import Thread
 
 motor_left = None
 motor_right = None
 encoder_left = None
 encoder_right = None
 
-lock = Lock()
-
-desired_vel = None
+desired_vel = (0.0, 0.0)
 curr_vel = [0.0, 0.0]
-prev_error = (0.0, 0.0)
-area = [0.0, 0.0]
-curr_eff = (0.0, 0.0)
 
 vel_pub = rospy.Publisher('wheel_vel', Float32MultiArray, queue_size= 1)
 
@@ -73,71 +68,62 @@ def drive_one_wheel(pwd, is_left):
     else:
         motor_right.forward(-pwd)
     
-def speed_controller(delta_t):
-    global desired_vel
-    global curr_vel
-    global prev_error
-    global area
-    global curr_eff
-
-    p = 0.3
-    i = 0.5
-    d = 0
-
-    desired_vl, desired_vr = desired_vel
-
-    left_eff = curr_eff[0]
-    right_eff = curr_eff[1]
-
-    curr_error = (desired_vl - curr_vel[0], desired_vr - curr_vel[1])
-
-    with lock:
-        area[0] = area[0] + ( 0.5 * (curr_error[0] + prev_error[0]) * delta_t)
-        area[1] = area[1] + ( 0.5 * (curr_error[1] + prev_error[1]) * delta_t)
-
-    l_proportion = curr_error[0] * p
-    l_integral = area[0] * i
-    l_derivative = ((curr_error[0] - prev_error[0]) / delta_t) * d
-
-    r_proportion = curr_error[1] * p
-    r_integral = area[1] * i
-    r_derivative = ((curr_error[1] - prev_error[1]) / delta_t) * d
-
-    left_eff = left_eff + l_proportion + l_integral + l_derivative
-    right_eff = right_eff + r_proportion + r_integral + r_derivative
-
-    with lock:
-        curr_eff = (left_eff, right_eff)
-        prev_error = curr_error
-
-    drive_one_wheel(left_eff, True)
-    drive_one_wheel(right_eff, False)
-
-def speed_controller_thread():
-    delta_t = 0.01
-    tolerance = 0.01
-    while not rospy.is_shutdown():
-        sleep(delta_t)
-        if (abs(prev_error[0]) > tolerance or abs(prev_error[1]) > tolerance) and desired_vel != None:
-            speed_controller(delta_t)
-        else:
-            desired_vel = None
-
 def update_desired(desired : Float32MultiArray):
     global desired_vel
-    global prev_error
-    global area
-    global curr_vel
     linear, angular = desired.data
     
     l = 0.101 # meters
     desired_vl = linear - ((angular * l)/2)
     desired_vr = linear + ((angular * l)/2)
 
-    with lock:
-        desired_vel = (desired_vl, desired_vr)
-        prev_error = (desired_vel[0] - curr_vel[0], desired_vel[1] - curr_vel[1])
-        area = [0.0, 0.0]
+    desired_vel = (desired_vl, desired_vr)
+
+def speed_controller():
+    global curr_vel
+    global desired_vel
+
+    tolerance = 0.05
+    delta_t = 0.01
+
+    p = 0.3
+    i = 0.5
+    d = 0
+    
+    curr_eff = (0.0, 0.0)
+    prev_desired = desired_vel
+
+    prev_error = (desired_vel[0] - curr_vel[0], desired_vel[1] - curr_vel[1])
+    area = (0.0, 0.0)
+
+    while not rospy.is_shutdown():
+        if prev_desired != desired_vel:
+            prev_error = (desired_vel[0] - curr_vel[0], desired_vel[1] - curr_vel[1])
+            area = (0.0, 0.0)
+            prev_desired = desired_vel
+
+        curr_error = (desired_vel[0] - curr_vel[0], desired_vel[1] - curr_vel[1])
+        area_l = area[0] + ( 0.5 * (curr_error[0] + prev_error[0]) * delta_t)
+        area_r = area[1] + ( 0.5 * (curr_error[1] + prev_error[1]) * delta_t)
+        area = (area_l, area_r)
+
+        l_proportion = curr_error[0] * p
+        l_integral = area[0] * i
+        l_derivative = ((curr_error[0] - prev_error[0]) / delta_t) * d
+
+        r_proportion = curr_error[1] * p
+        r_integral = area[1] * i
+        r_derivative = ((curr_error[1] - prev_error[1]) / delta_t) * d
+
+        left_eff = curr_eff[0] + l_proportion + l_integral + l_derivative
+        right_eff = curr_eff[1] + r_proportion + r_integral + r_derivative
+
+        curr_eff = (left_eff, right_eff)
+        prev_error = curr_error
+
+        drive_one_wheel(left_eff, True)
+        drive_one_wheel(right_eff, False)
+
+        sleep(delta_t)
 
 if __name__ == '__main__':
     delta_t = 0.01
@@ -145,7 +131,7 @@ if __name__ == '__main__':
     init()
     rospy.Subscriber("robot_twist", Float32MultiArray, update_desired)
 
-    speed_thread = Thread(target=speed_controller_thread)
+    speed_thread = Thread(target=speed_controller)
     speed_thread.start()
 
     prev_dist = get_distance()
