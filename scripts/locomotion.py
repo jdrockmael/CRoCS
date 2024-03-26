c31dd7a4c703e0f621dd98d17791432b0584b074
Tim-K3007 <oitsplazma444@gmail.com>
2024-03-26 14:12:05 -0400

    divided the vel and locomotion

--- scripts/locomotion.py
+++ scripts/locomotion.py
@@ -2,75 +2,16 @@
 import rospy
 from time import sleep
 from std_msgs.msg import Float32MultiArray
-from gpiozero import Device, PhaseEnableMotor, RotaryEncoder
-from gpiozero.pins.pigpio import PiGPIOFactory
-from math import pi
-from threading import Thread, Lock
-
-motor_left = None
-motor_right = None
-encoder_left = None
-encoder_right = None
+from threading import Lock
 
 desired_vel = (0.0, 0.0)
 curr_vel = [0.0, 0.0]
 
 desired_lock = Lock()
 vel_lock = Lock()
-
-vel_pub = rospy.Publisher('wheel_vel', Float32MultiArray, queue_size= 1)
-
-def init():
-    global motor_left 
-    global motor_right
-    global encoder_left
-    global encoder_right
-
-    Device.pin_factory = PiGPIOFactory()
-
-    motor_left = PhaseEnableMotor(24, 23)
-    motor_right = PhaseEnableMotor(21, 20)
-
-    encoder_left = RotaryEncoder(27, 22, max_steps = 256000)
-    encoder_right = RotaryEncoder(5, 6, max_steps = 256000)
-
-    motor_left.stop()
-    motor_right.stop()
-
-def get_distance():
-    tick_per_rev = 128.0
-    r_of_wheel = 0.021225 # in meters
-    left = (encoder_left.steps / tick_per_rev) * (2.0 * pi * r_of_wheel)
-    right = (-encoder_right.steps / tick_per_rev) * (2.0 * pi * r_of_wheel)
-
-    return (left, right)
-
-def calc_wheel_vel(prev_wheel_dis, curr_wheel_dis, delta_t):
-    delta_l = curr_wheel_dis[0] - prev_wheel_dis[0]
-    delta_r = curr_wheel_dis[1] - prev_wheel_dis[1]
-
-    left_vel = delta_l / delta_t
-    right_vel = delta_r / delta_t
-
-    return (left_vel, right_vel)
-
-def drive_one_wheel(pwd, is_left):
-    if pwd > 1:
-        pwd = 1
-    elif pwd < -1:
-        pwd = -1
-    elif pwd < 0.18 and pwd > -0.18:
-        pwd = 0
-
-    if pwd >= 0 and is_left:
-        motor_left.forward(pwd)
-    elif pwd < 0 and is_left:
-        motor_left.backward(-pwd)
-    elif pwd >= 0 and not is_left:
-        motor_right.backward(pwd)
-    else:
-        motor_right.forward(-pwd)
     
+eff_pub = rospy.Publisher("wheel_eff", Float32MultiArray, queue_size=1)
+
 def update_desired(desired : Float32MultiArray):
     global desired_vel
     linear, angular = desired.data
@@ -85,23 +26,10 @@ def update_desired(desired : Float32MultiArray):
     what = "setting global to", desired_vel
     rospy.logerr(what)
 
-def monitor_vel():
+def update_vel(wheel_vel : Float32MultiArray):
     global curr_vel
-    delta_t = 0.01
-
-    prev_dist = get_distance()
-    while not rospy.is_shutdown(): 
-        sleep(delta_t)
-        curr_dist = get_distance()
-
-        wheel_vel = calc_wheel_vel(prev_dist, curr_dist, delta_t)
-        prev_dist = curr_dist
-
-        with vel_lock:
-            curr_vel = wheel_vel
-
-        vel_pub.publish(Float32MultiArray(data=wheel_vel))
-        
+    with vel_lock:
+        curr_vel = wheel_vel.data
 
 def speed_controller():
     tolerance = 0.05
@@ -158,17 +86,12 @@ def speed_controller():
             curr_eff = (left_eff, right_eff)
             prev_error = curr_error
 
-            drive_one_wheel(left_eff, True)
-            drive_one_wheel(right_eff, False)
+            eff_pub.publish(Float32MultiArray(data=[left_eff, right_eff]))
 
         sleep(delta_t)
 
 if __name__ == '__main__':
     rospy.init_node('locomotion')
-    init()
     rospy.Subscriber("robot_twist", Float32MultiArray, update_desired)
-
-    vel_thread = Thread(target=monitor_vel)
-    vel_thread.start()
-
+    rospy.Subscriber("wheel_vel", Float32MultiArray, update_vel)
     speed_controller()
---
+++ scripts/vel_reader.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+import rospy
+from time import sleep
+from std_msgs.msg import Float32MultiArray
+from gpiozero import Device, PhaseEnableMotor, RotaryEncoder
+from gpiozero.pins.pigpio import PiGPIOFactory
+from math import pi
+
+motor_left = None
+motor_right = None
+encoder_left = None
+encoder_right = None
+
+vel_pub = rospy.Publisher('wheel_vel', Float32MultiArray, queue_size= 1)
+
+def init():
+    global motor_left 
+    global motor_right
+    global encoder_left
+    global encoder_right
+
+    Device.pin_factory = PiGPIOFactory()
+
+    motor_left = PhaseEnableMotor(24, 23)
+    motor_right = PhaseEnableMotor(21, 20)
+
+    encoder_left = RotaryEncoder(27, 22, max_steps = 256000)
+    encoder_right = RotaryEncoder(5, 6, max_steps = 256000)
+
+    motor_left.stop()
+    motor_right.stop()
+
+def get_distance():
+    tick_per_rev = 128.0
+    r_of_wheel = 0.021225 # in meters
+    left = (encoder_left.steps / tick_per_rev) * (2.0 * pi * r_of_wheel)
+    right = (-encoder_right.steps / tick_per_rev) * (2.0 * pi * r_of_wheel)
+
+    return (left, right)
+
+def calc_wheel_vel(prev_wheel_dis, curr_wheel_dis, delta_t):
+    delta_l = curr_wheel_dis[0] - prev_wheel_dis[0]
+    delta_r = curr_wheel_dis[1] - prev_wheel_dis[1]
+
+    left_vel = delta_l / delta_t
+    right_vel = delta_r / delta_t
+
+    return (left_vel, right_vel)
+
+def drive_one_wheel(pwd, is_left):
+    if pwd > 1:
+        pwd = 1
+    elif pwd < -1:
+        pwd = -1
+    elif pwd < 0.18 and pwd > -0.18:
+        pwd = 0
+
+    if pwd >= 0 and is_left:
+        motor_left.forward(pwd)
+    elif pwd < 0 and is_left:
+        motor_left.backward(-pwd)
+    elif pwd >= 0 and not is_left:
+        motor_right.backward(pwd)
+    else:
+        motor_right.forward(-pwd)
+
+def monitor_vel():
+    delta_t = 0.01
+
+    prev_dist = get_distance()
+    while not rospy.is_shutdown(): 
+        sleep(delta_t)
+        curr_dist = get_distance()
+
+        wheel_vel = calc_wheel_vel(prev_dist, curr_dist, delta_t)
+        prev_dist = curr_dist
+
+        vel_pub.publish(Float32MultiArray(data=wheel_vel))
+        
+
+def drive(effort : Float32MultiArray):
+    left, right = effort.data
+    drive_one_wheel(left, True)
+    drive_one_wheel(right, False)
+
+if __name__ == '__main__':
+    rospy.init_node('vel_reader')
+    init()
+    rospy.Subscriber("wheel_eff", Float32MultiArray, drive)
+    monitor_vel()
