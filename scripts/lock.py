95fa201fd585cc8df8174e668c2726954d10c0fa
Tim-K3007 <oitsplazma444@gmail.com>
2024-04-07 13:28:13 -0400

    changed up locking a lil so we can turn it off when needed

--- scripts/lock.py
+++ scripts/lock.py
@@ -4,17 +4,20 @@ from std_msgs.msg import Float32MultiArray
 from time import sleep
 
 speed_pub = rospy.Publisher("robot_twist", Float32MultiArray, queue_size=1)
+range_readings = (0.0, 0.0)
+lock_on = True
 
-def control_loop(cam : Float32MultiArray):
-    data = cam.data
-    delta_t = 0.05
-    distance_to_tag = 0.2
+def update_reading(cam : Float32MultiArray):
+    global range_readings
+    range_readings = (cam[2], cam[1])
 
-    if len(data) != 0:
-        heading_err = data[2]
-        distance_err = data[1] - distance_to_tag # in meters
+def control_loop():
+    heading_err = range_readings[2]
+    distance_err = range_readings[1] - 0.05 # in meters
+
+    if abs(distance_err) > 0.05 and abs(heading_err) > 0.01:
         
-        linear = distance_err * 5
+        linear = distance_err * 1
         angular_l = heading_err * 1
         angular_r = -heading_err * 1
 
@@ -23,11 +26,15 @@ def control_loop(cam : Float32MultiArray):
 
         speed_pub.publish(Float32MultiArray(data=[l_speed, r_speed]))
 
-        sleep(delta_t)
+        sleep(0.01)
     else:
         speed_pub.publish(Float32MultiArray(data=[0.0, 0.0]))
 
 if __name__ == '__main__':
     rospy.init_node('lock')
-    rospy.Subscriber("range", Float32MultiArray, control_loop)
-    rospy.spin()
+    rospy.Subscriber("range", Float32MultiArray, update_reading)
+
+    while not rospy.is_shutdown():
+        if lock_on:
+            control_loop()
+    
