167652893543102e77f1042719e1d579cbf11400
jdrockmael <jdrockmael@wpi.edu>
2024-04-11 12:33:59 -0400

    does this work?

--- rfcomm/bt_client.py
+++ rfcomm/bt_client.py
@@ -70,7 +70,7 @@ def rec_msg(socket):
             does = data.split(b'[')
             this = does[1].split(b']')
             work = this[0].split(b',')
-            x, y, theta = work[0], work[1], work[2]
+            x, y, theta = float(work[0]), float(work[1]), float(work[2])
             des_pos = [x, y, theta]
             print(x + y + theta)
 
