32a6c2be9e5871e48922799c17d5354f27fe42fa
Tim-K3007 <oitsplazma444@gmail.com>
2024-04-16 13:22:30 -0400

    now we can go to point or to a pose

--- scripts/ik.py
+++ scripts/ik.py
@@ -112,7 +112,9 @@ def drive_to_point(desired : Float32MultiArray):
 
     turn_to(desired_heading)
     drive_to(desired_pose)
-    turn_to(desired_heading)
+
+    if len(desired_pose) >= 3:
+        turn_to(desired_pose[2])
 
 if __name__ == '__main__':
     rospy.init_node('ik')
