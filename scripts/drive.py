40cb7c519b6e914db2d7204ae318ec1e88fc6013
Dang Tran <dttran@wpi.edu>
2024-04-17 12:35:08 -0400

    Cleanup?

--- scripts/drive.py
+++ scripts/drive.py
@@ -3,12 +3,10 @@ import rospy
 from geometry_msgs.msg import Twist
 import time
 import math
-from std_msgs.msg import Bool
 import numpy as np
-
 import math
-from gazebo_msgs.msg import ModelStates, LinkStates
-from tf.transformations import euler_from_quaternion, quaternion_from_euler
+from gazebo_msgs.msg import LinkStates
+from tf.transformations import euler_from_quaternion
 from std_msgs.msg import Int64, Int64MultiArray
 
 # Convert CBS coordinates to real world coordinates and vice versa. 39.37 for inches and meter conversion. 15 is for robot radius 3in*2 + 4.5in*2 cspace
@@ -18,7 +16,7 @@ def path2world(path_pos):
 def world2path(world_pos):
     return (float(world_pos) * 39.37 / 15.0)
 
-# Normalize angle between [-pi, pi]
+# Normalize angle between [-pi, pi]S
 def normalize(angle):
     angle = angle % (2 * np.pi)
     if angle > np.pi:             
@@ -33,68 +31,58 @@ class Main():
 
         self.path_state_pub = rospy.Publisher("/path_state", Int64MultiArray, queue_size=1)
         self.path_step_pub = rospy.Publisher("/path_step", Int64, queue_size=1)
+
         rospy.Subscriber("/path_state", Int64MultiArray, self.path_state_sub, queue_size=1)
         rospy.Subscriber("/path_step", Int64, self.path_step_sub, queue_size=1)
-
         rospy.Subscriber("/gazebo/link_states", LinkStates, self.true_sub, queue_size=1)
 
         self.link_id = 0
-        self.longest = False
         self.d = 0.039624
         self.path_state = [0] * 7
         self.time_step = -1
-        self.turn_speed = math.pi/16
-        self.forward_speed = 0.05
-        self.slow = self.cube_id / 10
+
+        self.turn_speed = math.pi/8
+        self.forward_speed = 0.2
+        self.slow = self.cube_id / 10       # Delay for cube state publishing to preventr overlaps
+
         self.rate = rospy.Rate(100)
                 
         time.sleep(5)
 
         if rospy.has_param("/cbs_output"):
-            self.paths = [None] * 7
-            for i in range(len(self.paths)):
-                self.paths[i] = rospy.get_param("/cbs_output/schedule/cube" + str(i+1))
-
-            self.path = self.paths[self.cube_id - 1]
+            self.cube_paths = rospy.get_param("/cbs_output/schedule")
+            self.path = self.cube_paths[str("cube" + str(self.cube_id))]
 
         rospy.spin()
 
     def path_state_sub(self, data):
         """ Subscriber for the current state of all robots
-        :data: Int64Arr, len = # of cubes in the system, 0 for beginning of time step, 1 for finished turning, and 2 for finished step
+        :data: Int64Arr, len = # of cubes in the system, 0 for beginning of time step, 1 for finished turning, 2 for finished step, 
+        3 for go forward and lock together, 4 for finished locking, 5 for crossing the gap, 6 for finished crossing, 7 for exploring
         """
         self.path_state = list(data.data)
+
         if self.path_state.count(3) == 7:
             self.locking()
             
         if self.path_state.count(5) == 7:
             self.drive(2.5, 0.1)
 
-            time.sleep(2)
+            time.sleep(self.slow)
+            # Signal finished crossing gap
             if self.cube_id == 1:
-                new_state = Int64MultiArray()
-                new_state.data = [6] * 7
-                self.path_state_pub.publish(new_state)
+                self.pub_state(6, True)
 
         if self.path_state.count(7) == 7:
-            time.sleep((30)/self.cube_id)
+            time.sleep(self.cube_id / 2)
 
             detach_dist = float(self.cube_id/7)
-            self.detach(detach_dist)
-
-    def detach(self, dist):
-        turn_angle = np.pi/2
-        if (self.cube_id % 2) == 0:
-            turn_angle = -turn_angle
-        self.drive(dist, 0.1)
-        self.turn(turn_angle)
-        self.drive(dist, 0.1)
+            self.explore(detach_dist)
 
     def locking(self):
-        turning_angle = - self.heading
+        self.turn(- self.heading)
 
-        self.turn(turning_angle)
-
+        # fuck this shite, could make it dynamic w apriltag reading
         if self.cube_id < 6:
             self.drive(0.24 * abs(self.cube_id-7), 0.05)
         elif self.cube_id < 5:
@@ -106,12 +94,21 @@ class Main():
 
         time.sleep(self.slow)
 
+        # Signal finished locking
         if self.cube_id == 1:
-            curr_state = self.path_state
-            curr_state[self.cube_id - 1] = 4
-            new_state = Int64MultiArray()
-            new_state.data = [4] * 7
-            self.path_state_pub.publish(new_state)
+            self.pub_state(4, True)
+    
+    def explore(self, dist):
+        """ After detaching, go around and "explore"
+        :dist: Distance in m to go forward after detaching
+        """
+
+        turn_angle = np.pi/2
+        if (self.cube_id % 2) == 0:
+            turn_angle = -turn_angle
+        self.drive(dist, 0.1)
+        self.turn(turn_angle)
+        self.drive(dist, 0.1)
             
 
     def path_step_sub(self, data):
@@ -135,11 +132,7 @@ class Main():
         time.sleep(self.slow)
 
         # Update path state to finished time step state (2)
-        curr_state = self.path_state
-        curr_state[self.cube_id - 1] = 2
-        new_state = Int64MultiArray()
-        new_state.data = curr_state
-        self.path_state_pub.publish(new_state)
+        self.pub_state(2, False)
 
     def drive_to_point(self, dest):
         """ Turns and drives to destinated goal location
@@ -161,22 +154,21 @@ class Main():
         delta_x, delta_y = self.dest[0] - self.pos[0], self.dest[1] - self.pos[1]
         distance = math.sqrt(delta_x**2 + delta_y**2)
 
+        # Correct itself before turning to account for offset wheelbase
         if self.time_step == (len(self.path) - 1):
             if self.heading > 0:
-                distance -= self.d
+                distance -= self.d 
             else:
                 distance += self.d
 
         # Wait until all cubes finished turning before start driving forward
-        while self.path_state.count(1) + self.path_state.count(2) < len(self.paths):
-            time.sleep(0.1)
+        while self.path_state.count(1) + self.path_state.count(2) < len(self.cube_paths):
+            time.sleep(0.01)
 
-        # Correct itself before turning to account for offset turning center
         if self.time_step+1 == len(self.path):
             distance -= self.d
 
         self.drive(distance, self.forward_speed)
-        # self.drive(path2world(distance), self.forward_speed)
             
     def drive(self, distance, speed):
         """ Drive the robot forward for the given distance at the given speed
@@ -197,11 +189,9 @@ class Main():
             self.rate.sleep()
 
         # Stop driving
-        msg = Twist()
-        msg.linear.x = 0.0  
-        msg.angular.z = 0.0
-        self.vel_pub.publish(msg)
+        self.stop()
 
+        # Variable wait to avoid overlapping between bots
         time.sleep(self.slow)
 
     def turn(self, angle):
@@ -220,27 +210,36 @@ class Main():
             self.rate.sleep()
 
         # Stop turning
-        msg = Twist()
-        msg.linear.x = 0.0  
-        msg.angular.z = 0.0
-        self.vel_pub.publish(msg)
+        self.stop()
 
         # Variable wait to avoid overlapping between bots
         time.sleep(self.slow)
 
         # Update path state to finished turning state (1)
-        curr_state = self.path_state
-        curr_state[self.cube_id - 1] = 1
+        self.pub_state(1, False)
+    
+    def stop(self):
+        msg = Twist()
+        msg.linear.x = 0.0  
+        msg.angular.z = 0.0
+        self.vel_pub.publish(msg)
+    
+    def pub_state(self, state, fillAll):
         new_state = Int64MultiArray()
-        new_state.data = curr_state
+
+        if fillAll:
+            new_state.data = [state] * len(self.cube_paths)
+        else:
+            curr_state = self.path_state
+            curr_state[self.cube_id - 1] = state
+            new_state.data = curr_state
+
         self.path_state_pub.publish(new_state)
-        time.sleep(self.slow)
 
     def true_sub(self, data):
         """ Publish ground-truth gazebo path 
         :data: LinkStates message from /gazebo/link_states
         """
-
         # Find index number of cube link
         if self.link_id == 0:
             for i, link in enumerate(data.name):
@@ -256,7 +255,6 @@ class Main():
 
         except IndexError:
             for i, link in enumerate(data.name):
-                # if link == str(self.cube + "::" + self.cube + "_dummy"):
                 if link == str("cube" + str(self.cube_id) + "::cube" + str(self.cube_id) + "_dummy"):
                     self.link_id = i
             pose = data.pose[self.link_id]
